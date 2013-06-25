0a10e7afac69cca15e5382db98e7669bd89b27a2
Mads Kristensen <post@madskristensen.net>
2013-06-25 12:00:13 -0700

    Added image preview to HTML editor

---
+++ EditorExtensions/QuickInfo/ImageHtml/ImageQuickInfoControllerProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace MadsKristensen.EditorExtensions.QuickInfo
+{
+    [Export(typeof(IIntellisenseControllerProvider))]
+    [Name("Image Html QuickInfo Controller")]
+    [ContentType("htmlx")]
+    internal class ImageHtmlQuickInfoControllerProvider : IIntellisenseControllerProvider
+    {
+        [Import, System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal IQuickInfoBroker QuickInfoBroker { get; set; }
+
+        public IIntellisenseController TryCreateIntellisenseController(ITextView textView, IList<ITextBuffer> subjectBuffers)
+        {
+            return new ImageHtmlQuickInfoController(textView, subjectBuffers, this);
+        }
+    }
+}
