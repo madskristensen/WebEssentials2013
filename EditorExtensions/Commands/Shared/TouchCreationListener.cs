96607fa7bb73b6d3cdb680a679016f369c4d14e4
Mads Kristensen <post@madskristensen.net>
2013-06-27 11:33:08 -0700

    SaveFileDialog for Angular controller smart tag

---
+++ EditorExtensions/Commands/Shared/TouchCreationListener.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace MadsKristensen.EditorExtensions
+{
+    [Export(typeof(IVsTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    class TouchCreationListener : IVsTextViewCreationListener
+    {
+        [Import, System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal IVsEditorAdaptersFactoryService EditorAdaptersFactoryService { get; set; }
+
+        [Import]
+        internal ICompletionBroker CompletionBroker { get; set; }
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            IWpfTextView view = EditorAdaptersFactoryService.GetWpfTextView(textViewAdapter);
+
+            view.VisualElement.TouchMove += VisualElement_TouchMove;
+        }
+
+        void VisualElement_TouchMove(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            //var input = (System.Windows.IInputElement)sender;
+
+            //System.Windows.Forms.MessageBox.Show(e.GetTouchPoint(input).Action);
+        }
+
+    }
+}
---
+++ EditorExtensions/SmartTags/HTML/HtmlAngularControllerSmartTag.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Html.Core;
+using Microsoft.Html.Editor.SmartTags;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MadsKristensen.EditorExtensions.SmartTags
+{
+    [Export(typeof(IHtmlSmartTagProvider))]
+    [ContentType(HtmlContentTypeDefinition.HtmlContentType)]
+    [Order(Before = "Default")]
+    [Name("HtmlAngularController")]
+    internal class HtmlAngularControllerSmartTagProvider : IHtmlSmartTagProvider
+    {
+        public IHtmlSmartTag TryCreateSmartTag(ITextView textView, ITextBuffer textBuffer, ElementNode element, AttributeNode attribute, int caretPosition, HtmlPositionType positionType)
+        {
+            if (element.GetAttribute("ng-controller") != null)
+            {
+                return new HtmlAngularControllerSmartTag(textView, textBuffer, element);
+            }
+
+            return null;
+        }
+    }
+
+    internal class HtmlAngularControllerSmartTag : HtmlSmartTag
+    {
+        public HtmlAngularControllerSmartTag(ITextView textView, ITextBuffer textBuffer, ElementNode element)
+            : base(textView, textBuffer, element, HtmlSmartTagPosition.ElementName)
+        {
+        }
+
+        protected override IEnumerable<ISmartTagAction> GetSmartTagActions(ITrackingSpan span)
+        {
+            return new ISmartTagAction[] { new FormatSelectionSmartTagAction(this) };
+        }
+
+        class FormatSelectionSmartTagAction : HtmlSmartTagAction
+        {
+            public FormatSelectionSmartTagAction(HtmlSmartTag htmlSmartTag) :
+                base(htmlSmartTag, "Add new Angular controller")
+            { }
+
+            public override void Invoke()
+            {
+                string value = HtmlSmartTag.Element.GetAttribute("ng-controller").Value;
+
+                if (string.IsNullOrEmpty(value))
+                    value = "myController";
+
+                string folder = ProjectHelpers.GetProjectFolder(EditorExtensionsPackage.DTE.ActiveDocument.FullName);
+                string file;
+
+                using (var dialog = new SaveFileDialog())
+                {
+                    dialog.FileName = value + ".js";
+                    dialog.DefaultExt = ".js";
+                    dialog.Filter = "JS files | *.js";
+                    dialog.InitialDirectory = folder;
+
+                    if (dialog.ShowDialog() != DialogResult.OK)
+                        return;
+
+                    file = dialog.FileName;
+                }                
+                                               
+                EditorExtensionsPackage.DTE.UndoContext.Open(this.DisplayText);
+                
+                string script = GetScript(value);
+                File.WriteAllText(file, script);
+                
+                ProjectHelpers.AddFileToActiveProject(file);
+                EditorExtensionsPackage.DTE.ItemOperations.OpenFile(file);
+
+                EditorExtensionsPackage.DTE.UndoContext.Close();
+            }
+
+            private static string GetScript(string value)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("var myApp = angular.module('myApp',[]);");
+                sb.AppendLine();
+                sb.AppendLine("myApp.controller('{0}', ['$scope', function($scope) {{");
+                sb.AppendLine("    $scope.greeting = 'Hola!';");
+                sb.AppendLine("}}]);");
+
+                string script = string.Format(sb.ToString(), value);
+                return script;
+            }
+        }
+    }
+}
