1bfaf227a5fb6ad79c9779ded3158ca38a5760f2
Schabse Laks <Dev@SLaks.net>
2013-07-05 12:48:16 -0400

    Add support for .jshintrc files
    If a .js file has a .jshintrc in any parent directory, that file will be
    used instead of the configuration in Web Essentials settings.
    
    If the .jshintrc is not valid Javascript, this will break.
    
    Fixes #11

--- EditorExtensions/Commands/JavaScript/JsHintCompiler.cs
+++ EditorExtensions/Commands/JavaScript/JsHintCompiler.cs
@@ -1,6 +1,7 @@
 ﻿using MadsKristensen.EditorExtensions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Threading;
@@ -11,22 +12,26 @@ using System.Windows.Threading;
 [ComVisible(true)]
 public class JsHintCompiler : ScriptRunnerBase
 {
-    private string _settings;
+    private string _defaultSettings;
     private JsHintOptions _options;
 
     public JsHintCompiler(Dispatcher dispatcher)
         : base(dispatcher)
     { }
 
-    protected override string CreateHtml(string source, string state)
+    protected override string CreateHtml(string source, string filename)
     {
+        // I override the meaning of this parameter to
+        // get the path so that I can find .jshintrc.
+        if (!File.Exists(filename))
+            throw new ArgumentException("The state parameter to Compile() must be the full path to the file being linted.", "filename");
         if (_options == null)
         {
             _options = new JsHintOptions();
             _options.LoadSettingsFromStorage();
-            JsHintOptions.Changed += delegate { _options.LoadSettingsFromStorage(); GenerateSettings(); };
+            JsHintOptions.Changed += delegate { _options.LoadSettingsFromStorage(); GenerateGlobalSettings(); };
 
-            GenerateSettings();
+            GenerateGlobalSettings();
         }
 
         source = source
@@ -36,13 +41,14 @@ public class JsHintCompiler : ScriptRunnerBase
             .Replace("'", "\\'");
 
         string script = ReadResourceFile("MadsKristensen.EditorExtensions.Resources.Scripts.jshint.js") +
-                        "JSHINT('" + source + "', {" + _settings + "});" +
-                        "window.external.Execute(JSON.stringify(JSHINT.errors), '" + state.Replace("\\", "\\\\") + "')";
+                        "var settings = " + (FindLocalSettings(filename) ?? "{" + _defaultSettings + "}") + ";" +   // If this file has no .jshintrc, fall back to the configured settings
+                        "JSHINT('" + source + "', settings, settings.globals);" +   // .jshintrc files have an optional globals section, which becomes the third parameter
+                        "window.external.Execute(JSON.stringify(JSHINT.errors), '" + filename.Replace("\\", "\\\\") + "')";
 
         return "<html><head><meta http-equiv=\"X-UA-Compatible\" content=\"IE=9\" /><script>" + script + "</script></head><html/>";
     }
 
-    private void GenerateSettings()
+    private void GenerateGlobalSettings()
     {
         Type type = _options.GetType();
         PropertyInfo[] properties = type.GetProperties();
@@ -67,7 +73,19 @@ public class JsHintCompiler : ScriptRunnerBase
             }
         }
 
-        _settings = string.Join(", ", list);
+        _defaultSettings = string.Join(", ", list);
+    }
+
+    private static string FindLocalSettings(string sourcePath)
+    {
+        string dir = Path.GetDirectoryName(sourcePath);
+        while(!File.Exists(Path.Combine(dir, ".jshintrc")))
+        {
+            dir = Path.GetDirectoryName(dir);
+            if (String.IsNullOrEmpty(dir))
+                return null;
+        }
+        return File.ReadAllText(Path.Combine(dir, ".jshintrc"));
     }
 }
 
