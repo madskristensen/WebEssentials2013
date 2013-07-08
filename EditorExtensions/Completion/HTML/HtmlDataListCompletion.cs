24b67c6c09ab55e80a8a6efb6b0bf44233818557
Mads Kristensen <post@madskristensen.net>
2013-07-08 13:45:50 -0700

    Added HTML Intellinsense for datalist and label

---
+++ EditorExtensions/Completion/HTML/HtmlDataListCompletion.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Html.Core;
+using Microsoft.Html.Editor.Intellisense;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor;
+using System;
+using System.Collections.Generic;
+
+namespace MadsKristensen.EditorExtensions
+{
+    [HtmlCompletionProvider(CompletionType.Values, "input", "list")]
+    [ContentType(HtmlContentTypeDefinition.HtmlContentType)]
+    public class HtmlDataListCompletion : IHtmlCompletionListProvider, IHtmlTreeVisitor
+    {
+        public CompletionType CompletionType
+        {
+            get { return CompletionType.Values; }
+        }
+
+        public IList<HtmlCompletion> GetEntries(HtmlCompletionContext context)
+        {
+            var result = new List<HtmlCompletion>();
+            var list = new List<string>();
+            var glyph = GlyphService.GetGlyph(StandardGlyphGroup.GlyphGroupVariable, StandardGlyphItem.GlyphItemPublic);
+
+            context.Document.HtmlEditorTree.RootNode.Accept(this, list);
+
+            foreach (var item in list)
+            {
+                var completion = new HtmlCompletion(item, item, item, glyph, HtmlIconAutomationText.AttributeIconText);
+                result.Add(completion);
+            }
+
+            return result;
+        }
+
+        public bool Visit(ElementNode element, object parameter)
+        {
+            if (element.Name.Equals("datalist", StringComparison.OrdinalIgnoreCase))
+            {
+                var list = (List<string>)parameter;
+
+                if (!list.Contains(element.Id))
+                    list.Add(element.Id);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ EditorExtensions/Completion/HTML/HtmlLabelForAttributeCompletion.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Html.Core;
+using Microsoft.Html.Editor.Intellisense;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor;
+using System.Collections.Generic;
+
+namespace MadsKristensen.EditorExtensions
+{
+    [HtmlCompletionProvider(CompletionType.Values, "label", "for")]
+    [ContentType(HtmlContentTypeDefinition.HtmlContentType)]
+    public class HtmlLabelForAttributeCompletion : IHtmlCompletionListProvider, IHtmlTreeVisitor
+    {
+        private static readonly List<string> _inputTypes = new List<string>() { "input", "textarea", "select" };
+        public CompletionType CompletionType
+        {
+            get { return CompletionType.Values; }
+        }
+
+        public IList<HtmlCompletion> GetEntries(HtmlCompletionContext context)
+        {
+            var result = new List<HtmlCompletion>();
+            var list = new List<string>();
+            var glyph = GlyphService.GetGlyph(StandardGlyphGroup.GlyphGroupVariable, StandardGlyphItem.GlyphItemPublic);
+
+            context.Document.HtmlEditorTree.RootNode.Accept(this, list);
+
+            foreach (var item in list)
+            {
+                var completion = new HtmlCompletion(item, item, item, glyph, HtmlIconAutomationText.AttributeIconText);
+                result.Add(completion);
+            }
+
+            return result;
+        }
+
+        public bool Visit(ElementNode element, object parameter)
+        {
+            if (_inputTypes.Contains(element.Name.ToLowerInvariant()))
+            {
+                var list = (List<string>)parameter;
+                var id = element.GetAttribute("id");
+
+                if (id != null && !list.Contains(id.Value))
+                    list.Add(id.Value);
+            }
+
+            return true;
+        }
+    }
+}
