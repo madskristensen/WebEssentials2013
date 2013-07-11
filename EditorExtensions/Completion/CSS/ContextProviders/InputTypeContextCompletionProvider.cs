a32aa7e852aac954eb4e2f5ec9f4786bc61186cb
Mads Kristensen <post@madskristensen.net>
2013-07-10 21:43:26 -0700

    Removed unused usings

--- EditorExtensions/Completion/CSS/ContextProviders/InputTypeContextCompletionProvider.cs
+++ EditorExtensions/Completion/CSS/ContextProviders/InputTypeContextCompletionProvider.cs
@@ -5,9 +5,6 @@ using Microsoft.VisualStudio.Utilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MadsKristensen.EditorExtensions.Completion.ContextProviders
 {
