cd8232fe05865bde4949cca4901c31b85202e099
Mads Kristensen <post@madskristensen.net>
2013-08-23 13:33:06 -0700

    Cleaned up gzip method a bit

--- EditorExtensions/Commands/Css/CssSaveListener.cs
+++ EditorExtensions/Commands/Css/CssSaveListener.cs
@@ -85,22 +85,23 @@ namespace MadsKristensen.EditorExtensions
         public static byte[] Compress(string text)
         {
             byte[] buffer = Encoding.UTF8.GetBytes(text);
-            MemoryStream ms = new MemoryStream();
-            using (GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true))
+
+            using (MemoryStream ms = new MemoryStream())
             {
-                zip.Write(buffer, 0, buffer.Length);
+                using (GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true))
+                {
+                    zip.Write(buffer, 0, buffer.Length);
+                }
+
+                ms.Position = 0;
+                byte[] compressed = new byte[ms.Length];
+                ms.Read(compressed, 0, compressed.Length);
+
+                byte[] gzBuffer = new byte[compressed.Length + 4];
+                System.Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
+                System.Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
+                return gzBuffer;
             }
-
-            ms.Position = 0;
-            MemoryStream outStream = new MemoryStream();
-
-            byte[] compressed = new byte[ms.Length];
-            ms.Read(compressed, 0, compressed.Length);
-
-            byte[] gzBuffer = new byte[compressed.Length + 4];
-            System.Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
-            System.Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
-            return gzBuffer;
         }
     }
 }
