01c040a2ae267325c41c8d5a11b29a0975028f5e
Mike Lorbetske <mike.lorbetske@n3-p.com>
2013-09-12 10:34:02 -0700

    Better remove rule behavior

--- EditorExtensions/SmartTags/CSS/Actions/RemoveCssRuleSmartTagAction.cs
+++ EditorExtensions/SmartTags/CSS/Actions/RemoveCssRuleSmartTagAction.cs
@@ -24,31 +24,73 @@ namespace MadsKristensen.EditorExtensions.SmartTags.CSS.Actions
             EditorExtensionsPackage.DTE.UndoContext.Open(DisplayText);
             var snapshot = _span.TextBuffer.CurrentSnapshot;
             var position = _rule.Start + _rule.Length;
-            var end = CalculateEndPositionToTrimLineEndings(snapshot, position);
-            var ss = new SnapshotSpan(snapshot, _rule.Start, end - _rule.Start);
+            var start = CalculateDeletionStartFromStartPosition(snapshot, _rule.Start);
+            var end = CalculateDeletionEndFromRuleEndPosition(snapshot, position);
+            var length = end - start;
+            var ss = new SnapshotSpan(snapshot, start, length);
             _span.TextBuffer.Delete(ss);
 
             EditorExtensionsPackage.DTE.UndoContext.Close();
         }
 
-        private static int CalculateEndPositionToTrimLineEndings(ITextSnapshot snapshot, int startPosition)
+        private static int CalculateDeletionStartFromStartPosition(ITextSnapshot snapshot, int startPosition)
         {
-            var position = startPosition;
+            var position = startPosition - 1;
+
+            if (position < 0)
+            {
+                return 0;
+            }
+
+            while (true)
+            {
+                if (position > 0)
+                {
+                    var ss = new SnapshotSpan(snapshot, position, 1);
+                    var text = ss.GetText();
+
+                    if (text != null && !"\r\n".Contains(text) && string.IsNullOrWhiteSpace(text))
+                    {
+                        --position;
+                        continue;
+                    }
+                    
+                    ++position;
+                }
+
+                return position;
+            }
+        }
+
+        private static int CalculateDeletionEndFromRuleEndPosition(ITextSnapshot snapshot, int endPosition)
+        {
+            var position = endPosition;
+            var committedPosition = position;
 
             while (true)
             {
                 if (position < snapshot.Length)
                 {
                     var ss = new SnapshotSpan(snapshot, position, 1);
+                    var text = ss.GetText();
 
-                    if (string.IsNullOrWhiteSpace(ss.GetText()))
+                    if (text != null)
                     {
-                        ++position;
-                        continue;
+                        if ("\r\n".Contains(text))
+                        {
+                            committedPosition = ++position;
+                            continue;
+                        }
+                        
+                        if (string.IsNullOrWhiteSpace(text))
+                        {
+                            ++position;
+                            continue;
+                        }
                     }
                 }
 
-                return position;
+                return committedPosition;
             }
         }
     }
