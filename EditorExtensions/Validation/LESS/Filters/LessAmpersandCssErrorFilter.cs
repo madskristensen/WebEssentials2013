00f3e033ca81e0e1b9905047431d7d1ec38b462a
Mads Kristensen <post@madskristensen.net>
2013-10-04 17:50:50 -0700

    Fixed LESS validator to ignore &

---
+++ EditorExtensions/Validation/LESS/Filters/LessAmpersandCssErrorFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CSS.Core;
+using Microsoft.Less.Core;
+using Microsoft.VisualStudio.Utilities;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace MadsKristensen.EditorExtensions
+{
+    [Export(typeof(ICssErrorFilter))]
+    [Name("LessAmpersandCssErrorFilter")]
+    [Order(After = "Default")]
+    internal class LessAmpersandCssErrorFilter : ICssErrorFilter
+    {
+        public void FilterErrorList(IList<ICssError> errors, ICssCheckerContext context)
+        {
+            for (int i = errors.Count - 1; i > -1; i--)
+            {
+                ICssError error = errors[i];
+                LessDeclaration dec = error.Item as LessDeclaration;
+
+                if (dec != null && dec.Text.Contains("&"))
+                {
+                    errors.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
