f267b221edd7ff3ea68c49d3829a4014db202fd9
Schabse Laks <Dev@SLaks.net>
2013-10-05 21:27:50 -0400

    JSHint: Add empty Ignore parser

---
+++ EditorExtensions/Commands/JavaScript/JsHintIgnoreList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadsKristensen.EditorExtensions.Commands.JavaScript
+{
+    ///<summary>Contains a parsed representation of a .jshintignore file.</summary>
+    class JsHintIgnoreList
+    {
+        //legacy.js
+        //somelib/**
+        //otherlib/*.js
+
+
+        public static JsHintIgnoreList Parse(TextReader reader)
+        {
+            return null;
+        }
+        public static JsHintIgnoreList Parse(string text) { return Parse(new StringReader(text)); }
+        public static JsHintIgnoreList Load(string path) { using (var reader = File.OpenText(path)) return Parse(reader); }
+    }
+}
