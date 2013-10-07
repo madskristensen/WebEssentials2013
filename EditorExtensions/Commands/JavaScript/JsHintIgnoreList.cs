221b38c1a6ea1c3cfbde59b5f004076452804bad
Schabse Laks <Dev@SLaks.net>
2013-10-07 12:37:54 -0400

    Use Minimatch to duplicate JSHint's ignore logic

--- EditorExtensions/Commands/JavaScript/JsHintIgnoreList.cs
+++ EditorExtensions/Commands/JavaScript/JsHintIgnoreList.cs
@@ -1,25 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Minimatch;
 
 namespace MadsKristensen.EditorExtensions.Commands.JavaScript
 {
     ///<summary>Contains a parsed representation of a .jshintignore file.</summary>
     class JsHintIgnoreList
     {
-        //legacy.js
-        //somelib/**
-        //otherlib/*.js
+        readonly ReadOnlyCollection<Minimatcher> matchers;
 
-
-        public static JsHintIgnoreList Parse(TextReader reader)
+        public JsHintIgnoreList(IEnumerable<Minimatcher> matchers)
         {
-            return null;
+            this.matchers = matchers.ToList().AsReadOnly();
         }
-        public static JsHintIgnoreList Parse(string text) { return Parse(new StringReader(text)); }
-        public static JsHintIgnoreList Load(string path) { using (var reader = File.OpenText(path)) return Parse(reader); }
+
+        // Based on https://github.com/jshint/jshint/blob/83374ad/src/cli.js#L168-L219
+
+        public bool IsIgnored(string path)
+        {
+            path = Path.GetFullPath(path);
+            return matchers.Any(m => m.IsMatch(path));
+        }
+
+        public static JsHintIgnoreList Load(string path)
+        {
+            string parentPath = Path.GetDirectoryName(path);
+            return new JsHintIgnoreList(
+                File.ReadLines(path)
+                    .Where(p => !String.IsNullOrWhiteSpace(p))
+                    .Select(p => new Minimatcher(
+                        PrefixedResolve(parentPath, path.Trim()),
+                        new Minimatch.Options { AllowWindowsPaths = true, IgnoreCase = true })
+                )
+            );
+        }
+
+        static string PrefixedResolve(string parentPath, string str)
+        {
+            if (str[0] == '!')
+                return "!" + Path.GetFullPath(Path.Combine(parentPath, str.Substring(1)));
+            return Path.GetFullPath(Path.Combine(parentPath, str));
+        }
     }
 }
