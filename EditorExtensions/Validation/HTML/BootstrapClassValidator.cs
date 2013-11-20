794641f5bb1398a525f8383cfe51a5cbecf8add0
Adeel <adeelbm@outlook.com>
2013-11-20 22:22:23 +0300

    Fixed #240

--- EditorExtensions/Validation/HTML/BootstrapClassValidator.cs
+++ EditorExtensions/Validation/HTML/BootstrapClassValidator.cs
@@ -17,6 +17,7 @@ namespace MadsKristensen.EditorExtensions
     public class BootstrapClassValidator : BaseValidator
     {
         private static string[] _tokens = new[] { "btn", "glyphicon", "alert", "label", "fa" }; // fa is for FontAwesome
+        private static string[] _childDependentTokes = new[] { "fa" };
         private static string _error = "When using \"{0}\", you must also specify the class \"{1}\".";
         private static Dictionary<string, string[]> _whitelist = new Dictionary<string, string[]>
         {
@@ -27,13 +28,16 @@ namespace MadsKristensen.EditorExtensions
         {
             var results = new ValidationErrorCollection();
             var classNames = element.GetAttribute("class");
+            List<string> childrenClassNames = null;
 
             if (classNames == null)
                 return results;
 
             foreach (string token in _tokens)
             {
-                if (!IsCorrect(classNames.Value, token))
+                childrenClassNames = GetIfTokenIsDependent(token, element);
+
+                if (!IsCorrect(classNames.Value, token, childrenClassNames))
                 {
                     int index = element.Attributes.IndexOf(classNames);
                     string offender = GetOffendingClassName(classNames.Value, token);
@@ -46,10 +50,27 @@ namespace MadsKristensen.EditorExtensions
             return results;
         }
 
-        private static bool IsCorrect(string input, string token)
+        private List<string> GetIfTokenIsDependent(string token, ElementNode element)
+        {
+            List<string> childrenClassNames = null;
+
+            if (_childDependentTokes.Any(tk => token.StartsWith(tk)))
+            {
+                childrenClassNames = new List<string>();
+
+                foreach (var child in element.Children)
+                {
+                    childrenClassNames.Add(child.GetAttribute("class") == null ? child.GetAttribute("class").Value : "");
+                }
+            }
+            return childrenClassNames;
+        }
+
+        private static bool IsCorrect(string input, string token, List<string> childrenClassNames = null)
         {
             if (input.Contains(token + "-") &&
-                !(input.Contains(token + " ") || input.EndsWith(token) || IsWhitelisted(input, token)))
+                 !(input.Contains(token + " ") || input.EndsWith(token) || IsWhitelisted(input, token)) &&
+                ((childrenClassNames != null && !childrenClassNames.All(cn => cn.Contains(token + " ") || cn.EndsWith(token))) || childrenClassNames == null))
                 return false;
 
             return true;
---
+++ WebEssentialsTests/BootStrapValidationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using MadsKristensen.EditorExtensions;
+using Microsoft.CSS.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Html.Core;
+
+namespace WebEssentialsTests
+{
+    [TestClass]
+    public class BootstrapValidationTests
+    {
+        [TestMethod]
+        public void PathCompilationTest()
+        {
+            BootstrapClassValidator validator = new BootstrapClassValidator();
+
+            /* Not sure how to create ElementNode from string */
+
+           // ElementNode node = new ElementNode();
+          //  IList<IHtmlValidationError> compiled = validator.ValidateElement(); 
+            
+            //  int expected = 0;
+
+          //  Assert.AreEqual(expected, compiled.Count);
+        }
+
+    }
+}
