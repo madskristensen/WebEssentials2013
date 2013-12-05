0fa8ea43fd45928f350d9f890bd799df90a6ac28
Schabse Laks <Dev@SLaks.net>
2013-12-04 20:14:36 -0500

    Markdown: Start Outliner tests
    Doesn't work yet

---
+++ WebEssentialsTests/MarkdownOutlinerTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using MadsKristensen.EditorExtensions.Classifications.Markdown;
+using Microsoft.Html.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace WebEssentialsTests
+{
+    [TestClass]
+    public class MarkdownOutlinerTests
+    {
+        static void TestOutlines(string markdown, params Tuple<string, string[]>[] expectedOutlines)
+        {
+            var artifacts = new ArtifactCollection(new MarkdownCodeArtifactProcessor());
+            artifacts.Build(markdown);
+
+            var snapshot = new MockSnapshot(markdown.Replace("{[", "").Replace("]}", ""));
+
+            var expected = new List<TagSpan<IOutliningRegionTag>>();
+            var lastIndex = 0;
+            for (int i = 0; i < expectedOutlines.Length; i++)
+            {
+                var spanStart = markdown.IndexOf("{[", lastIndex);
+                markdown = markdown.Remove(spanStart, 2);
+                var spanEnd = markdown.IndexOf("]}", spanStart);
+                markdown = markdown.Remove(spanEnd, 2);
+                expected.Add(new TagSpan<IOutliningRegionTag>(
+                    new SnapshotSpan(snapshot, spanStart, spanEnd),
+                    new SimpleOutlineTag(expectedOutlines[i])
+                ));
+                lastIndex = spanEnd;
+            }
+            if (markdown != snapshot.GetText())
+                throw new ArgumentException("Unexpected test delimiters");
+
+
+            var tagger = new MarkdownOutlineTagger(artifacts, (c, t) => new SimpleOutlineTag(c, t));
+            var actual = tagger.GetTags(new NormalizedSnapshotSpanCollection(new SnapshotSpan(
+                new MockSnapshot(markdown),
+                new Span(0, markdown.Length)
+            )));
+            actual.ShouldAllBeEquivalentTo(expected);
+        }
+
+        class SimpleOutlineTag : IOutliningRegionTag
+        {
+            public SimpleOutlineTag(Tuple<string, string[]> tuple)
+            {
+                CollapsedText = tuple.Item1;
+                HintLines = tuple.Item2;
+            }
+
+            public SimpleOutlineTag(object collapsed, IEnumerable<SnapshotSpan> lines)
+            {
+                CollapsedText = collapsed.ToString();
+                HintLines = lines.Select(s => s.GetText()).ToList();
+            }
+            public string CollapsedText { get; private set; }
+            public IEnumerable<string> HintLines { get; private set; }
+            object IOutliningRegionTag.CollapsedForm { get { return CollapsedText; } }
+            object IOutliningRegionTag.CollapsedHintForm { get { return HintLines; } }
+            bool IOutliningRegionTag.IsDefaultCollapsed { get { return false; } }
+            bool IOutliningRegionTag.IsImplementation { get { return true; } }
+
+            public override bool Equals(object obj)
+            {
+                var tag = obj as SimpleOutlineTag;
+                return tag != null
+                    && EqualityComparer<string>.Default.Equals(CollapsedText, tag.CollapsedText)
+                    && EqualityComparer<string>.Default.Equals(String.Join(Environment.NewLine, HintLines), String.Join(Environment.NewLine, tag.HintLines));
+            }
+
+            public override int GetHashCode()
+            {
+                var hashCode = EqualityComparer<string>.Default.GetHashCode(CollapsedText);
+                hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(String.Join(Environment.NewLine, HintLines));
+                return hashCode;
+            }
+        }
+
+        [TestMethod]
+        public void TestCodeBlocks()
+        {
+            TestOutlines(@"`code`");
+            TestOutlines(@"`code` `more`
+`even more inline code blocks should not be outlined`");
+            TestOutlines(@"
+    Single indented line should not be outlines");
+            TestOutlines(@"{[```
+First line
+Second line
+```]}", Tuple.Create("[ Code Block ]", new[] { "First line", "Second line" }));
+            TestOutlines(@"{[```html
+First line
+Second line
+```]}", Tuple.Create("[ html Code Block ]", new[] { "First line", "Second line" }));
+            TestOutlines(@"{[
+    First line
+    Second line
+]}", Tuple.Create("[ Code Block ]", new[] { "First line", "Second line" }));
+        }
+        // TODO: Test quoted code blocks, test overlapping partial spans.
+    }
+}
