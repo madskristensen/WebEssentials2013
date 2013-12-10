b512d404923982b79a10366dc3989d007634b279
Schabse Laks <Dev@SLaks.net>
2013-12-10 08:24:27 -0500

    Markdown: Fix preview pane
    Get ContentType from document TextBuffer
    instead of primary (projection) TextBuffer.
    
    Also cleanup code.

--- EditorExtensions/Margin/EditorMarginFactory.cs
+++ EditorExtensions/Margin/EditorMarginFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
@@ -19,30 +21,27 @@ namespace MadsKristensen.EditorExtensions
         [Import]
         public ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
 
+        static readonly Dictionary<string, Func<string, ITextDocument, IWpfTextViewMargin>> marginFactories = new Dictionary<string, Func<string, ITextDocument, IWpfTextViewMargin>>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "LESS",           (source, document) => new LessMargin("CSS", source, WESettings.GetBoolean(WESettings.Keys.ShowLessPreviewWindow), document) },
+            { "CoffeeScript",   (source, document) => new CoffeeScriptMargin("JavaScript", source, WESettings.GetBoolean(WESettings.Keys.ShowCoffeeScriptPreviewWindow), document) },
+            { "Markdown",       (source, document) => new MarkdownMargin("text", source, WESettings.GetBoolean(WESettings.Keys.MarkdownShowPreviewWindow), document) }
+        };
+
         public IWpfTextViewMargin CreateMargin(IWpfTextViewHost wpfTextViewHost, IWpfTextViewMargin marginContainer)
         {
-            string source = wpfTextViewHost.TextView.TextBuffer.CurrentSnapshot.GetText();
+            Func<string, ITextDocument, IWpfTextViewMargin> creator;
+            if (!marginFactories.TryGetValue(wpfTextViewHost.TextView.TextDataModel.DocumentBuffer.ContentType.TypeName, out creator))
+                return null;
+
             ITextDocument document;
 
-            if (TextDocumentFactoryService.TryGetTextDocument(wpfTextViewHost.TextView.TextDataModel.DocumentBuffer, out document))
-            {
-                switch (wpfTextViewHost.TextView.TextBuffer.ContentType.DisplayName.ToLowerInvariant())
-                {
-                    case "less":
-                        bool showLess = WESettings.GetBoolean(WESettings.Keys.ShowLessPreviewWindow);
-                        return new LessMargin("CSS", source, showLess, document);
+            if (!TextDocumentFactoryService.TryGetTextDocument(wpfTextViewHost.TextView.TextDataModel.DocumentBuffer, out document))
+                return null;
 
-                    case "coffeescript":
-                        bool showCoffee = WESettings.GetBoolean(WESettings.Keys.ShowCoffeeScriptPreviewWindow);
-                        return new CoffeeScriptMargin("JavaScript", source, showCoffee, document);
+            string source = wpfTextViewHost.TextView.TextBuffer.CurrentSnapshot.GetText();
 
-                    case "markdown":
-                        bool showMarkdown = WESettings.GetBoolean(WESettings.Keys.MarkdownShowPreviewWindow);
-                        return new MarkdownMargin("text", source, showMarkdown, document);
-                }
-            }
-
-            return null;
+            return creator(source, document);
         }
     }
 }
