6737875caa97b4c9d973baa31b83dbb647ecf655
Adeel <adeelbm@outlook.com>
2013-12-11 05:08:26 +0300

    Analysis: (CA1719) Parameter matching member name.

--- EditorExtensions/Completion/HTML/StaticListCompletion.cs
+++ EditorExtensions/Completion/HTML/StaticListCompletion.cs
@@ -8,33 +8,33 @@ namespace MadsKristensen.EditorExtensions
 {
     public abstract class StaticListCompletion : IHtmlCompletionListProvider
     {
+        private readonly IReadOnlyDictionary<string, IList<HtmlCompletion>> values;
+        protected static readonly ReadOnlyCollection<HtmlCompletion> Empty = new ReadOnlyCollection<HtmlCompletion>(new HtmlCompletion[0]);
+
         public CompletionType CompletionType
         {
             get { return CompletionType.Values; }
         }
+        ///<summary>Gets the property that serves as a key to look up completion values against.</summary>
+        protected abstract string KeyProperty { get; }
 
         protected StaticListCompletion(Dictionary<string, IList<HtmlCompletion>> values)
         {
             this.values = values;
         }
 
-        ///<summary>Gets the property that serves as a key to look up completion values against.</summary>
-        protected abstract string KeyProperty { get; }
-
-        protected static readonly ReadOnlyCollection<HtmlCompletion> Empty = new ReadOnlyCollection<HtmlCompletion>(new HtmlCompletion[0]);
-
         ///<summary>Creates a collection of HTML completion items from a list of static values.</summary>
-        protected static ReadOnlyCollection<HtmlCompletion> Values(params string[] values)
+        protected static ReadOnlyCollection<HtmlCompletion> Values(params string[] staticValues)
         {
-            return new ReadOnlyCollection<HtmlCompletion>(Array.ConvertAll(values, s => new SimpleHtmlCompletion(s)));
-        }
-        ///<summary>Creates a collection of HTML completion items from a list of static values.</summary>
-        protected static ReadOnlyCollection<HtmlCompletion> Values(IEnumerable<string> values)
-        {
-            return new ReadOnlyCollection<HtmlCompletion>(values.Select(s => new SimpleHtmlCompletion(s)).ToList<HtmlCompletion>());
+            return new ReadOnlyCollection<HtmlCompletion>(Array.ConvertAll(staticValues, s => new SimpleHtmlCompletion(s)));
         }
 
-        readonly IReadOnlyDictionary<string, IList<HtmlCompletion>> values;
+        ///<summary>Creates a collection of HTML completion items from a list of static values.</summary>
+        protected static ReadOnlyCollection<HtmlCompletion> Values(IEnumerable<string> staticValues)
+        {
+            return new ReadOnlyCollection<HtmlCompletion>(staticValues.Select(s => new SimpleHtmlCompletion(s)).ToList<HtmlCompletion>());
+        }
+
         public IList<HtmlCompletion> GetEntries(HtmlCompletionContext context)
         {
             IList<HtmlCompletion> result;
