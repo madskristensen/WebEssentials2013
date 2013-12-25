661983363342db564512e9d702cc90c1af6ff867
Schabse Laks <Dev@SLaks.net>
2013-12-25 09:02:33 -0500

    Code: Fix JSHint issues in generated Javascript
    Use literals where appropriate

--- EditorExtensions/Commands/Code/IntellisenseWriter.cs
+++ EditorExtensions/Commands/Code/IntellisenseWriter.cs
@@ -68,12 +68,12 @@ namespace MadsKristensen.EditorExtensions
 
                 foreach (var p in io.Properties)
                 {
-                    string value = p.Type.JavaScriptName + (p.Type.IsArray ? "[]" : "");
+                    string type = p.Type.JavaScriptName + (p.Type.IsArray ? "[]" : "");
                     var propertyName = CamelCasePropertyName(p.Name);
                     comment = p.Summary ?? "The " + propertyName + " property as defined in " + io.FullName;
                     comment = whitespaceTrimmer.Replace(comment, " ");
-                    sb.AppendLine("\t/// <field name=\"" + propertyName + "\" type=\"" + value + "\">" + SecurityElement.Escape(comment) + "</field>");
-                    sb.AppendLine("\tthis." + propertyName + " = new " + value + "();");
+                    sb.AppendLine("\t/// <field name=\"" + propertyName + "\" type=\"" + type + "\">" + SecurityElement.Escape(comment) + "</field>");
+                    sb.AppendLine("\tthis." + propertyName + " = " + p.Type.JavaScripLiteral + ";");
                 }
 
                 sb.AppendLine("};");
@@ -219,6 +219,30 @@ namespace MadsKristensen.EditorExtensions
         {
             get { return GetTargetName(false); }
         }
+
+        public string JavaScripLiteral {
+            get
+            {
+                if (IsArray)
+                    return "[]";
+                switch (JavaScriptName) 
+                {
+                    case "Number":
+                        return "0";
+                    case "String":
+                        return "''";
+                    case "Boolean":
+                        return "false";
+                    case "Array":
+                        return "[]";
+                    case "Object":
+                        return "{ }";
+                    default:
+                        return "new " + JavaScriptName + "()";
+                }
+            }
+        }
+
 
         string GetTargetName(bool js)
         {
