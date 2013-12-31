34a98c5938cba422fa247b9e4ca5437436ff4143
Mads Kristensen <post@madskristensen.net>
2013-12-31 09:13:57 -0800

    CoffeeScript: Indent on newline. Fixed #184

---
+++ EditorExtensions/Commands/CoffeeScript/EnterIndentationCommandTarget.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace MadsKristensen.EditorExtensions
+{
+    internal class EnterIndentation : CommandTargetBase
+    {
+        private Regex _indent = new Regex(@"^([\s]+)", RegexOptions.Compiled);
+
+        public EnterIndentation(IVsTextView adapter, IWpfTextView textView)
+            : base(adapter, textView, typeof(Microsoft.VisualStudio.VSConstants.VSStd2KCmdID).GUID, 3)
+        { }
+
+        protected override bool Execute(uint commandId, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+            int position = TextView.Caret.Position.BufferPosition.Position;
+            SnapshotPoint point = new SnapshotPoint(TextView.TextBuffer.CurrentSnapshot, position);
+            IWpfTextViewLine line = TextView.GetTextViewLineContainingBufferPosition(point);
+
+            string text = TextView.TextBuffer.CurrentSnapshot.GetText(line.Start, line.Length);
+
+            Match match = _indent.Match(text);
+
+            if (match.Success)
+            {
+                EditorExtensionsPackage.DTE.UndoContext.Open("Smart Indent");
+
+                TextView.TextBuffer.Insert(position, Environment.NewLine + match.Value);
+
+                EditorExtensionsPackage.DTE.UndoContext.Close();
+                return true;
+            }
+            
+            return false;
+        }
+
+        protected override bool IsEnabled()
+        {
+            return true;
+        }
+    }
+}
