eca16548ad0e88d89de0df3035a6a70d0e77d5d5
Mads Kristensen <post@madskristensen.net>
2013-12-31 11:10:20 -0800

    Diff files: Fixed issue #385

--- EditorExtensions/MenuItems/Diff.cs
+++ EditorExtensions/MenuItems/Diff.cs
@@ -16,42 +16,24 @@ namespace MadsKristensen.EditorExtensions
         public void SetupCommands()
         {
             CommandID commandId = new CommandID(GuidList.guidDiffCmdSet, (int)PkgCmdIDList.cmdDiff);
-            OleMenuCommand menuCommand = new OleMenuCommand((s, e) => Sort(), commandId);
-            menuCommand.BeforeQueryStatus += menuCommand_BeforeQueryStatus;
+            OleMenuCommand menuCommand = new OleMenuCommand((s, e) => PerformDiff(), commandId);
+            menuCommand.BeforeQueryStatus += BeforeQueryStatus;
             _mcs.AddCommand(menuCommand);
         }
 
-        //private List<string> list = new List<string>()
-        //{
-        //    ".txt", ".cs", ".aspx", ".ascx", ".asmx", ".master", ".cshtml", ".vbhtml", ".js", ".coffee", ".css", ".less", ".sass", ".scss", ".xml"
-        //};
-
         private List<string> files;
 
-        void menuCommand_BeforeQueryStatus(object sender, System.EventArgs e)
+        private void BeforeQueryStatus(object sender, System.EventArgs e)
         {
             OleMenuCommand menuCommand = sender as OleMenuCommand;
             files = new List<string>(ProjectHelpers.GetSelectedItemPaths());
 
-            //if (files.Count == 2)
-            //{
-            //    if (list.Contains(Path.GetExtension(files[0]).ToLowerInvariant()))
-            //    {
-            //        if (list.Contains(Path.GetExtension(files[1]).ToLowerInvariant()))
-            //        {
-            //            menuCommand.Enabled = true;
-            //            return;
-            //        }
-            //    }
-            //}
-
             menuCommand.Enabled = files.Count == 2;
         }
 
-        private void Sort()
+        private void PerformDiff()
         {
-            if (files.Count == 2)
-                EditorExtensionsPackage.ExecuteCommand("Tools.DiffFiles \"" + files[0] + "\" \"" + files[1] + "\"");
+            EditorExtensionsPackage.DTE.ExecuteCommand("Tools.DiffFiles", "\"" + files[0] + "\" \"" + files[1] + "\"");
         }
     }
 }
