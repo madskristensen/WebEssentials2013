5fedc35222d53f666b6a2b8c1afc36e59f80327e
Mads Kristensen <post@madskristensen.net>
2014-01-02 08:19:24 -0800

    Markdown: Added additional file extensions

--- EditorExtensions/Classifications/Markdown/MarkdownContentTypeDefinition.cs
+++ EditorExtensions/Classifications/Markdown/MarkdownContentTypeDefinition.cs
@@ -35,5 +35,20 @@ namespace MadsKristensen.EditorExtensions.Classifications.Markdown
         [ContentType(MarkdownContentType)]
         [FileExtension(".markdown")]
         public FileExtensionToContentTypeDefinition MarkdownFileExtension { get; set; }
+
+        [Export(typeof(FileExtensionToContentTypeDefinition))]
+        [ContentType(MarkdownContentType)]
+        [FileExtension(".mkd")]
+        public FileExtensionToContentTypeDefinition MkdFileExtension { get; set; }
+
+        [Export(typeof(FileExtensionToContentTypeDefinition))]
+        [ContentType(MarkdownContentType)]
+        [FileExtension(".mkdn")]
+        public FileExtensionToContentTypeDefinition MkdnFileExtension { get; set; }
+
+        [Export(typeof(FileExtensionToContentTypeDefinition))]
+        [ContentType(MarkdownContentType)]
+        [FileExtension(".mdwn")]
+        public FileExtensionToContentTypeDefinition MdwnFileExtension { get; set; }
     }
 }
