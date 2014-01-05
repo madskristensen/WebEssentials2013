36042d101c636a2ef54d83002b886c0d5785e4ea
Mads Kristensen <post@madskristensen.net>
2014-01-04 17:17:28 -0800

    SASS: Added ContentType definition
    It inherits from the CSS content type

---
+++ EditorExtensions/Classifications/SASS/SassContentTypeDefinition.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace MadsKristensen.EditorExtensions
+{
+    /// <summary>
+    /// Exports the Sass content type and file extension
+    /// </summary>
+    public class SassContentTypeDefinition
+    {
+        public const string SassContentType = "Sass";
+
+        /// <summary>
+        /// Exports the Sass content type
+        /// </summary>
+        [Export(typeof(ContentTypeDefinition))]
+        [Name(SassContentType)]
+        [BaseDefinition("CSS")]
+        public ContentTypeDefinition ISassContentType { get; set; }
+
+        [Export(typeof(FileExtensionToContentTypeDefinition))]
+        [ContentType(SassContentType)]
+        [FileExtension(".sass")]
+        public FileExtensionToContentTypeDefinition SassFileExtension { get; set; }
+    }
+}
