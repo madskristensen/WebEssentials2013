3619322db53fae51cee8acc7ac76e97370ddc0f2
Mads Kristensen <post@madskristensen.net>
2014-01-10 12:33:25 -0800

    Test: Removed unneeded regex from LESS testhelper

--- WebEssentialsTests/Tests/LESS/LessCompilationTests.cs
+++ WebEssentialsTests/Tests/LESS/LessCompilationTests.cs
@@ -16,7 +16,6 @@ namespace WebEssentialsTests
     {
         private static string originalPath;
         private static readonly string BaseDirectory = Path.GetDirectoryName(typeof(NodeModuleImportedTests).Assembly.Location);
-        private static readonly Regex _linesStartingWithTwoSpaces = new Regex("(\n( *))", RegexOptions.Compiled);
 
         #region Helper Methods
         private static async Task<string> CompileLess(string fileName, string targetFileName)
@@ -25,8 +24,7 @@ namespace WebEssentialsTests
 
             if (result.IsSuccess)
             {
-                // Insert extra line-breaks between adjecent rule (to mimic the compiler's post-processing)
-                File.WriteAllText(targetFileName, _linesStartingWithTwoSpaces.Replace(File.ReadAllText(targetFileName).Trim(), "$1$2"));
+                File.WriteAllText(targetFileName, File.ReadAllText(targetFileName).Trim());
 
                 return result.Result;
             }
