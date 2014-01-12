553ebe735153de2976dc48d1a1e0324764110073
Mads Kristensen <post@madskristensen.net>
2014-01-12 13:56:17 -0800

    Lint: Made fix for config file encoding issue

---
+++ EditorExtensions/Compilers/Lint/LintConfigEncodingFix.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Composition;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace MadsKristensen.EditorExtensions.Commands
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    public class LintConfigEncodingFixViewCreationListener : IWpfTextViewCreationListener
+    {
+        [Import]
+        public ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            ITextDocument document;
+
+            if (TextDocumentFactoryService.TryGetTextDocument(textView.TextBuffer, out document))
+            {
+                string fileName = Path.GetFileName(document.FilePath);
+
+                if (fileName != ".jshintrc" && fileName != "tslint.json")
+                    return;
+
+                document.Encoding = new UTF8Encoding(false);
+            }
+        }
+    }
+}
