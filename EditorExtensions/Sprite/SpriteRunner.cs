d6ba7a9a6de47b83259da5ae4ef00f2c38e21325
Adeel <adeelbm@outlook.com>
2014-01-12 18:10:45 +0300

    Feature: Implement sprite image. (Issue #466) Using https://github.com/am11/RectanglePacker Usage:
    // Using System.Linq, Mapper
    // imageFileCollection is IEnumerable<string>
    var rectangles =
           imageFileCollection.Select(path =>
               {
               var image = Image.FromFile(path);
               return new ImageInfo(
                    (int)image.Width,
                    (int)image.Height,
                    path);
               }
           );
    
    // destinationSpriteFile is string
    // (desired/path/to/sprite.png)
    new SpriteRunner(rectangles)
      .GenerateSpriteWithMaps(destinationSpriteFile);
    
    // Now destinationSpriteFile (.png) has image,
    // and .sprite is JSON-based map.
    // Call optimizer on destinationSpriteFile

---
+++ EditorExtensions/Sprite/SpriteRunner.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web.Helpers;
+using Mapper;
+
+namespace MadsKristensen.EditorExtensions
+{
+    /// <summary>
+    /// Sprite runner class
+    /// </summary>
+    public class SpriteRunner
+    {
+        IEnumerable<ImageInfo> rectangles;
+
+        private int _imageZoom = 1;
+
+        public SpriteRunner(IEnumerable<ImageInfo> imageInfo)
+        {
+            rectangles = imageInfo;
+        }
+
+        /// <summary>
+        /// Generate Sprite image with map file
+        /// </summary>
+        /// <param name="spriteImageFile">Full path to destination file name</param>
+        public void GenerateSpriteWithMaps(string spriteImageFile)
+        {
+            Mapper.Canvas _canvas = new Mapper.Canvas();
+            MapperOptimalEfficiency<Sprite> mapper = new MapperOptimalEfficiency<Sprite>(_canvas);
+
+            Sprite sprite = mapper.Mapping(rectangles);
+
+            SpriteToImage(sprite, spriteImageFile);
+
+            // Write .sprite file
+            var spriteName = Path.ChangeExtension(spriteImageFile, ".sprite");
+            dynamic spriteInfo = null;
+
+            spriteInfo.File = spriteImageFile;
+            spriteInfo.Constituent = rectangles;
+
+            FileHelpers.WriteFile(Json.Encode(spriteInfo), spriteName);
+        }
+
+
+        /// <summary>
+        /// Creates an image showing the structure of a sprite (that is, its constituent images).
+        /// </summary>
+        /// <param name="sprite">
+        /// Sprite to be shown
+        /// </param>
+        /// <param name="spriteFileName">
+        /// Generate Sprite image with map file
+        /// </param>
+        /// <returns>
+        /// Url of the generated image.
+        /// </returns>
+        private void SpriteToImage(ISprite sprite, string spriteImageFile)
+        {
+            using (Bitmap bitmap = new Bitmap(sprite.Width * _imageZoom, sprite.Height * _imageZoom))
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    // Draw border around the entire image
+                    DrawRectangle(
+                        graphics,
+                        sprite.Width * _imageZoom, sprite.Height * _imageZoom,
+                        0, 0,
+                        false, false);
+
+                    foreach (IMappedImageInfo mappedImageInfo in sprite.MappedImages)
+                    {
+                        DrawRectangle(
+                            graphics,
+                            mappedImageInfo.ImageInfo.Width * _imageZoom, mappedImageInfo.ImageInfo.Height * _imageZoom,
+                            mappedImageInfo.X * _imageZoom, mappedImageInfo.Y * _imageZoom,
+                            true, false);
+                    }
+                }
+
+                bitmap.Save(spriteImageFile, ImageFormat.Png);
+            }
+        }
+
+
+        /// <summary>
+        /// Draws a rectangle on the canvas. The rectangle denotes a FreeArea.
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="xOffset"></param>
+        /// <param name="yOffset"></param>
+        /// <param name="occupied"></param>
+        private static void DrawRectangle(Graphics graphics, int width, int height, int xOffset, int yOffset, bool occupied, bool newlyOccupied)
+        {
+            // Fill the rectangle
+            Color c;
+            if (occupied)
+            {
+                if (newlyOccupied)
+                {
+                    c = Color.DarkGreen;
+                }
+                else
+                {
+                    c = Color.LightGreen;
+                }
+            }
+            else
+            {
+                c = Color.White;
+            }
+
+            SolidBrush brush = new SolidBrush(c);
+            graphics.FillRectangle(brush, xOffset, yOffset, width, height);
+
+            // Draw border 
+            Pen pen = new Pen(Color.Black);
+            graphics.DrawRectangle(pen, xOffset, yOffset, width - 1, height - 1);
+        }
+    }
+}
