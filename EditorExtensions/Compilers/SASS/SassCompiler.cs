928ac61cc5c1fb92c776adcb2c61d0782559feec
Schabse Laks <Dev@SLaks.net>
2014-01-14 18:09:22 -0500

    Compilers: Extract LESS & SASS SourceMap code
    Includes ugly hack for hcatlin/libsass#242

--- EditorExtensions/Compilers/SASS/SassCompiler.cs
+++ EditorExtensions/Compilers/SASS/SassCompiler.cs
@@ -10,24 +10,26 @@ using MadsKristensen.EditorExtensions.Helpers;
 using Microsoft.CSS.Core;
 using Microsoft.VisualStudio.Utilities;
 
-namespace MadsKristensen.EditorExtensions
+namespace MadsKristensen.EditorExtensions.Compilers
 {
     [ContentType("SASS")]
-    public class SassCompiler : NodeExecutorBase
+    public class SassCompiler : CssCompilerBase
     {
         private static readonly string _compilerPath = Path.Combine(WebEssentialsResourceDirectory, @"nodejs\tools\node_modules\node-sass\bin\node-sass");
         private static readonly Regex _errorParsingPattern = new Regex(@"(?<fileName>.*):(?<line>.\d*): error: (?<message>.*\n.*)", RegexOptions.Multiline);
-        private static readonly Regex _sourceMapInCss = new Regex(@"\/\*#([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*\/", RegexOptions.Multiline);
 
         public override string ServiceName { get { return "SASS"; } }
         public override string TargetExtension { get { return ".css"; } }
+
         protected override string CompilerPath { get { return _compilerPath; } }
         protected override Regex ErrorParsingPattern { get { return _errorParsingPattern; } }
+
+        protected override bool GenerateSourceMap { get { return WESettings.Instance.Sass.GenerateSourceMaps; } }
         protected override string GetArguments(string sourceFileName, string targetFileName)
         {
             var args = new StringBuilder();
 
-            if (WESettings.Instance.Sass.GenerateSourceMaps)
+            if (GenerateSourceMap)
             {
                 args.Append("--source-map ");
             }
@@ -36,78 +38,10 @@ namespace MadsKristensen.EditorExtensions
 
             return args.ToString();
         }
-
-        protected override string PostProcessResult(string resultSource, string sourceFileName, string targetFileName)
+        //https://github.com/hcatlin/libsass/issues/242
+        protected override string ReadMapFile(string sourceMapFilename)
         {
-            resultSource = UpdateSourceMapUrls(resultSource, targetFileName);
-
-            var message = "SASS: " + Path.GetFileName(sourceFileName) + " compiled.";
-
-            // If the caller wants us to renormalize URLs to a different filename, do so.
-            if (targetFileName != null && resultSource.IndexOf("url(", StringComparison.OrdinalIgnoreCase) > 0)
-            {
-                try
-                {
-                    resultSource = CssUrlNormalizer.NormalizeUrls(
-                        tree: new CssParser().Parse(resultSource, true),
-                        targetFile: targetFileName,
-                        oldBasePath: sourceFileName
-                    );
-                }
-                catch (Exception ex)
-                {
-                    message = "SASS: An error occurred while normalizing generated paths in " + sourceFileName + "\r\n" + ex;
-                }
-            }
-
-            Logger.Log(message);
-
-            return resultSource;
-        }
-
-        private static string UpdateSourceMapUrls(string content, string compiledFileName)
-        {
-            if (WESettings.Instance.Sass.GenerateSourceMaps || !File.Exists(compiledFileName))
-                return content;
-
-            string sourceMapFilename = compiledFileName + ".map";
-
-            if (!File.Exists(sourceMapFilename))
-                return content;
-
-            var updatedFileContent = GetUpdatedSourceMapFileContent(compiledFileName, sourceMapFilename);
-
-            if (updatedFileContent == null)
-                return content;
-
-            FileHelpers.WriteFile(updatedFileContent, sourceMapFilename);
-            ProjectHelpers.AddFileToProject(compiledFileName, sourceMapFilename);
-
-            return UpdateSourceLinkInCssComment(content, FileHelpers.RelativePath(compiledFileName, sourceMapFilename));
-        }
-
-        private static string GetUpdatedSourceMapFileContent(string cssFileName, string sourceMapFilename)
-        {
-            // Should be removed when this is fixed: https://github.com/hcatlin/libsass/issues/242
-            var sourceMapFileContent = File.ReadAllText(sourceMapFilename).Replace("\\", "\\\\");
-
-            // Read JSON map file and deserialize.
-            dynamic jsonSourceMap = Json.Decode(sourceMapFileContent);
-
-            if (jsonSourceMap == null)
-                return null;
-
-            jsonSourceMap.sources = ((IEnumerable<dynamic>)jsonSourceMap.sources).Select(s => FileHelpers.RelativePath(cssFileName, s));
-            jsonSourceMap.names = new List<dynamic>(jsonSourceMap.names);
-            jsonSourceMap.file = Path.GetFileName(cssFileName);
-
-            return Json.Encode(jsonSourceMap);
-        }
-
-        private static string UpdateSourceLinkInCssComment(string content, string sourceMapRelativePath)
-        {   // Fix sourceMappingURL comment in CSS file with network accessible path.
-            return _sourceMapInCss.Replace(content,
-                string.Format(CultureInfo.CurrentCulture, "/*# sourceMappingURL={0} */", sourceMapRelativePath));
+            return File.ReadAllText(sourceMapFilename).Replace("\\", "\\\\");
         }
     }
 }
