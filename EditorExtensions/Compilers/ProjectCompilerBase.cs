c50d1665a62d531157c30aa8e016d29caf4d568c
Mads Kristensen <post@madskristensen.net>
2014-01-15 15:18:47 -0800

    LESS: Don't compile when no .css. Fixed #497

--- EditorExtensions/Compilers/ProjectCompilerBase.cs
+++ EditorExtensions/Compilers/ProjectCompilerBase.cs
@@ -32,8 +32,12 @@ namespace MadsKristensen.EditorExtensions
             return Task.WhenAll(sourceFiles.Select(async sourceFile =>
             {
                 string compiledFile = MarginBase.GetCompiledFileName(sourceFile, CompileToExtension, CompileToLocation);
+                
+                if (!File.Exists(compiledFile))
+                    return;
+                
                 var result = await Compiler.Compile(sourceFile, compiledFile);
-
+                
                 if (result == null)
                     return;
 
