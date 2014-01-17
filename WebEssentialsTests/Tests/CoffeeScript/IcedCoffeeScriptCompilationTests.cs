bec77143f377e375d8aec25f8e588edb90ddbd16
Adeel <adeelbm@outlook.com>
2014-01-17 23:19:33 +0300

    Tests: Change fixtures; now passing all tests.

--- WebEssentialsTests/Tests/CoffeeScript/IcedCoffeeScriptCompilationTests.cs
+++ WebEssentialsTests/Tests/CoffeeScript/IcedCoffeeScriptCompilationTests.cs
@@ -31,7 +31,7 @@ namespace WebEssentialsTests
                 // Skip the version header, so we don't need
                 // to update the expecation for CS releases.
                 var compiledLines = compiledCode.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries).Skip(1);
-                var expectedLines = File.ReadLines(compiledFile).Skip(1).Where(s => !string.IsNullOrEmpty(s));
+                var expectedLines = File.ReadAllText(compiledFile).Replace("\r", "").Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries).Skip(1); ;
 
                 compiledLines.Should().Equal(expectedLines);
             }
