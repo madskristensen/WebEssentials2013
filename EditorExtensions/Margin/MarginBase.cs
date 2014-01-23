5b994332a98cb20edeead8879106ef53d3b9ac32
wjerla <wjerla@discovertec.com>
2014-01-23 11:56:53 -0500

    Adjusted tabs / spacing

--- EditorExtensions/Margin/MarginBase.cs
+++ EditorExtensions/Margin/MarginBase.cs
@@ -10,196 +10,197 @@ using Microsoft.Win32;
 
 namespace MadsKristensen.EditorExtensions
 {
-		///<summary>A base class for all margins, providing  basic layout and resize functionality.</summary>
-		public abstract class MarginBase : DockPanel, IWpfTextViewMargin
+	///<summary>A base class for all margins, providing  basic layout and resize functionality.</summary>
+	public abstract class MarginBase : DockPanel, IWpfTextViewMargin
+	{
+		readonly string _settingsKey;
+		FrameworkElement _previewControl;
+
+		private bool _isDisposed = false;
+		private Dispatcher _dispatcher;
+		protected ITextDocument Document { get; private set; }
+
+		protected IMarginSettings Settings { get; private set; }
+
+		protected MarginBase(IMarginSettings settings, ITextDocument document)
 		{
-				readonly string _settingsKey;
-				FrameworkElement _previewControl;
+			_dispatcher = Dispatcher.CurrentDispatcher;
+			_settingsKey = document.TextBuffer.ContentType.DisplayName + "Margin_width";
+			Document = document;
+			Settings = settings;
 
-				private bool _isDisposed = false;
-				private Dispatcher _dispatcher;
-				protected ITextDocument Document { get; private set; }
-
-				protected IMarginSettings Settings { get; private set; }
-
-				protected MarginBase(IMarginSettings settings, ITextDocument document)
-				{
-						_dispatcher = Dispatcher.CurrentDispatcher;
-						_settingsKey = document.TextBuffer.ContentType.DisplayName + "Margin_width";
-						Document = document;
-						Settings = settings;
-
-						if (settings.ShowPreviewPane)
-						{
-								_dispatcher.BeginInvoke(
-										new Action(CreateMarginControls), DispatcherPriority.ApplicationIdle, null);
-						}
-				}
-
-				protected abstract FrameworkElement CreatePreviewControl(double width);
-
-				protected virtual void CreateMarginControls()
-				{
-						int width;
-						using (var key = EditorExtensionsPackage.Instance.UserRegistryRoot)
-						{
-								var raw = key.GetValue("WE_" + _settingsKey);
-								width = raw is int ? (int)raw : -1;
-						}
-						width = width == -1 ? 400 : width;
-
-						Grid grid = new Grid();
-						grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(0, GridUnitType.Star) });
-						grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(5, GridUnitType.Pixel) });
-						grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(width, GridUnitType.Pixel) });
-						grid.RowDefinitions.Add(new RowDefinition());
-
-						_previewControl = CreatePreviewControl(400);
-						grid.Children.Add(_previewControl);
-						Children.Add(grid);
-
-						Grid.SetColumn(_previewControl, 2);
-						Grid.SetRow(_previewControl, 0);
-
-						GridSplitter splitter = new GridSplitter();
-						splitter.Width = 5;
-						splitter.ResizeDirection = GridResizeDirection.Columns;
-						splitter.VerticalAlignment = VerticalAlignment.Stretch;
-						splitter.HorizontalAlignment = HorizontalAlignment.Stretch;
-						splitter.DragCompleted += splitter_DragCompleted;
-
-						grid.Children.Add(splitter);
-						Grid.SetColumn(splitter, 1);
-						Grid.SetRow(splitter, 0);
-				}
-
-				void splitter_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
-				{
-						if (double.IsNaN(_previewControl.ActualWidth)) return;
-						using (var key = EditorExtensionsPackage.Instance.UserRegistryRoot)
-						{
-								key.SetValue("WE_" + _settingsKey, _previewControl.ActualWidth, RegistryValueKind.DWord);
-						}
-				}
-
-				private void ThrowIfDisposed()
-				{
-						if (_isDisposed)
-								throw new ObjectDisposedException(GetType().Name);
-				}
-				#region IWpfTextViewMargin Members
-
-				/// <summary>
-				/// The <see cref="Sytem.Windows.FrameworkElement"/> that implements the visual representation
-				/// of the margin.
-				/// </summary>
-				public FrameworkElement VisualElement { get { return this; } }
-
-				#endregion
-
-				#region ITextViewMargin Members
-
-				public double MarginSize
-				{
-						// Since this is a horizontal margin, its width will be bound to the width of the text view.
-						// Therefore, its size is its height.
-						get
-						{
-								ThrowIfDisposed();
-								return ActualHeight;
-						}
-				}
-
-				public bool Enabled { get { return true; } }
-
-				/// <summary>
-				/// Returns an instance of the margin if this is the margin that has been requested.
-				/// </summary>
-				/// <param name="marginName">The name of the margin requested</param>
-				/// <returns>An instance of EditorMargin1 or null</returns>
-				public ITextViewMargin GetTextViewMargin(string marginName)
-				{
-						return (marginName == GetType().Name) ? this : null;
-				}
-
-
-				///<summary>Releases all resources used by the MarginBase.</summary>
-				public void Dispose() { Dispose(true); GC.SuppressFinalize(this); }
-				///<summary>Releases the unmanaged resources used by the MarginBase and optionally releases the managed resources.</summary>
-				///<param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
-				protected virtual void Dispose(bool disposing)
-				{
-						if (disposing)
-						{
-						}
-				}
-				#endregion
+			if (settings.ShowPreviewPane)
+			{
+				_dispatcher.BeginInvoke(
+						new Action(CreateMarginControls), DispatcherPriority.ApplicationIdle, null);
+			}
 		}
 
-		///<summary>A base class for margins that use the original file contents rather than the output of a compiler.</summary>
-		public abstract class DirectMarginBase : MarginBase
+		protected abstract FrameworkElement CreatePreviewControl(double width);
+
+		protected virtual void CreateMarginControls()
 		{
-				protected DirectMarginBase(IMarginSettings settings, ITextDocument document)
-						: base(settings, document)
-				{
-						Document.FileActionOccurred += Document_FileActionOccurred;
-				}
-
-				private void Document_FileActionOccurred(object sender, TextDocumentFileActionEventArgs e)
-				{
-						if (e.FileActionType == FileActionTypes.ContentSavedToDisk)
-								UpdateMargin(e.FilePath);
-				}
-
-				protected override void CreateMarginControls()
-				{
-						base.CreateMarginControls();
-						Dispatcher.InvokeAsync(() => UpdateMargin(Document.FilePath));
-				}
-
-				///<summary>Updates the contents of the margin.</summary>
-				protected abstract void UpdateMargin(string filePath);
+			int width;
+			using (var key = EditorExtensionsPackage.Instance.UserRegistryRoot)
+			{
+				var raw = key.GetValue("WE_" + _settingsKey);
+				width = raw is int ? (int)raw : -1;
+			}
+			width = width == -1 ? 400 : width;
 
 
-				///<summary>Releases the unmanaged resources used by the DirectMarginBase and optionally releases the managed resources.</summary>
-				///<param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
-				protected override void Dispose(bool disposing)
-				{
-						if (disposing)
-						{
-								Document.FileActionOccurred -= Document_FileActionOccurred;
-						}
-						base.Dispose(disposing);
-				}
+			Grid grid = new Grid();
+			grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(0, GridUnitType.Star) });
+			grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(5, GridUnitType.Pixel) });
+			grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(width, GridUnitType.Pixel) });
+			grid.RowDefinitions.Add(new RowDefinition());
+
+			_previewControl = CreatePreviewControl(400);
+			grid.Children.Add(_previewControl);
+			Children.Add(grid);
+
+			Grid.SetColumn(_previewControl, 2);
+			Grid.SetRow(_previewControl, 0);
+
+			GridSplitter splitter = new GridSplitter();
+			splitter.Width = 5;
+			splitter.ResizeDirection = GridResizeDirection.Columns;
+			splitter.VerticalAlignment = VerticalAlignment.Stretch;
+			splitter.HorizontalAlignment = HorizontalAlignment.Stretch;
+			splitter.DragCompleted += splitter_DragCompleted;
+
+			grid.Children.Add(splitter);
+			Grid.SetColumn(splitter, 1);
+			Grid.SetRow(splitter, 0);
 		}
-		///<summary>A base class for margins that display the result of an <see cref="ICompilationNotifier"/>.</summary>
-		public abstract class CompilingMarginBase : MarginBase
+
+		void splitter_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
 		{
-				public ICompilationNotifier Notifier { get; private set; }
-				protected CompilingMarginBase(IMarginSettings settings, ITextDocument document)
-						: base(settings, document)
-				{
-						Notifier = Mef.GetImport<ICompilationNotifierProvider>(Document.TextBuffer.ContentType).GetCompilationNotifier(document);
-						Notifier.CompilationReady += (s, e) => UpdateMargin(e.CompilerResult);
-				}
-
-				protected override void CreateMarginControls()
-				{
-						base.CreateMarginControls();
-						Dispatcher.InvokeAsync(() => Notifier.RequestCompilationResult(cached: true));
-				}
-				protected abstract void UpdateMargin(CompilerResult result);
-
-
-				///<summary>Releases the unmanaged resources used by the CompilingMarginBase and optionally releases the managed resources.</summary>
-				///<param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
-				protected override void Dispose(bool disposing)
-				{
-						if (disposing)
-						{
-								Notifier.Dispose();
-						}
-						base.Dispose(disposing);
-				}
+			if (double.IsNaN(_previewControl.ActualWidth)) return;
+			using (var key = EditorExtensionsPackage.Instance.UserRegistryRoot)
+			{
+				key.SetValue("WE_" + _settingsKey, _previewControl.ActualWidth, RegistryValueKind.DWord);
+			}
 		}
+
+		private void ThrowIfDisposed()
+		{
+			if (_isDisposed)
+				throw new ObjectDisposedException(GetType().Name);
+		}
+		#region IWpfTextViewMargin Members
+
+		/// <summary>
+		/// The <see cref="Sytem.Windows.FrameworkElement"/> that implements the visual representation
+		/// of the margin.
+		/// </summary>
+		public FrameworkElement VisualElement { get { return this; } }
+
+		#endregion
+
+		#region ITextViewMargin Members
+
+		public double MarginSize
+		{
+			// Since this is a horizontal margin, its width will be bound to the width of the text view.
+			// Therefore, its size is its height.
+			get
+			{
+				ThrowIfDisposed();
+				return ActualHeight;
+			}
+		}
+
+		public bool Enabled { get { return true; } }
+
+		/// <summary>
+		/// Returns an instance of the margin if this is the margin that has been requested.
+		/// </summary>
+		/// <param name="marginName">The name of the margin requested</param>
+		/// <returns>An instance of EditorMargin1 or null</returns>
+		public ITextViewMargin GetTextViewMargin(string marginName)
+		{
+			return (marginName == GetType().Name) ? this : null;
+		}
+
+
+		///<summary>Releases all resources used by the MarginBase.</summary>
+		public void Dispose() { Dispose(true); GC.SuppressFinalize(this); }
+		///<summary>Releases the unmanaged resources used by the MarginBase and optionally releases the managed resources.</summary>
+		///<param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+			}
+		}
+		#endregion
+	}
+
+	///<summary>A base class for margins that use the original file contents rather than the output of a compiler.</summary>
+	public abstract class DirectMarginBase : MarginBase
+	{
+		protected DirectMarginBase(IMarginSettings settings, ITextDocument document)
+			: base(settings, document)
+		{
+			Document.FileActionOccurred += Document_FileActionOccurred;
+		}
+
+		private void Document_FileActionOccurred(object sender, TextDocumentFileActionEventArgs e)
+		{
+			if (e.FileActionType == FileActionTypes.ContentSavedToDisk)
+				UpdateMargin(e.FilePath);
+		}
+
+		protected override void CreateMarginControls()
+		{
+			base.CreateMarginControls();
+			Dispatcher.InvokeAsync(() => UpdateMargin(Document.FilePath));
+		}
+
+		///<summary>Updates the contents of the margin.</summary>
+		protected abstract void UpdateMargin(string filePath);
+
+
+		///<summary>Releases the unmanaged resources used by the DirectMarginBase and optionally releases the managed resources.</summary>
+		///<param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				Document.FileActionOccurred -= Document_FileActionOccurred;
+			}
+			base.Dispose(disposing);
+		}
+	}
+	///<summary>A base class for margins that display the result of an <see cref="ICompilationNotifier"/>.</summary>
+	public abstract class CompilingMarginBase : MarginBase
+	{
+		public ICompilationNotifier Notifier { get; private set; }
+		protected CompilingMarginBase(IMarginSettings settings, ITextDocument document)
+			: base(settings, document)
+		{
+			Notifier = Mef.GetImport<ICompilationNotifierProvider>(Document.TextBuffer.ContentType).GetCompilationNotifier(document);
+			Notifier.CompilationReady += (s, e) => UpdateMargin(e.CompilerResult);
+		}
+
+		protected override void CreateMarginControls()
+		{
+			base.CreateMarginControls();
+			Dispatcher.InvokeAsync(() => Notifier.RequestCompilationResult(cached: true));
+		}
+		protected abstract void UpdateMargin(CompilerResult result);
+
+
+		///<summary>Releases the unmanaged resources used by the CompilingMarginBase and optionally releases the managed resources.</summary>
+		///<param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				Notifier.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
