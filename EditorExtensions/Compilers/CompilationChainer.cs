cbe98d4123bec9f648a9627810e959d8397f4408
Schabse Laks <Dev@SLaks.net>
2014-02-26 09:10:53 -0500

    LESS: Re-instate broken code
    Reverts commenting out; does not compile.
    Next commit will fix compilation errors for clean diffs.

--- EditorExtensions/Compilers/CompilationChainer.cs
+++ EditorExtensions/Compilers/CompilationChainer.cs
@@ -1,77 +1,77 @@
-﻿//using System.Collections.Generic;
-//using System.ComponentModel.Composition;
-//using System.IO;
-//using MadsKristensen.EditorExtensions.Helpers;
-//using Microsoft.VisualStudio.Editor;
-//using Microsoft.VisualStudio.Text;
-//using Microsoft.VisualStudio.Text.Editor;
-//using Microsoft.VisualStudio.TextManager.Interop;
-//using Microsoft.VisualStudio.Utilities;
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using MadsKristensen.EditorExtensions.Helpers;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
 
-//namespace MadsKristensen.EditorExtensions.Compilers
-//{
-//    [Export(typeof(IVsTextViewCreationListener))]
-//    [TextViewRole(PredefinedTextViewRoles.Document)]
-//    [ContentType("LESS")]
-//    //[ContentType("SASS")]
+namespace MadsKristensen.EditorExtensions.Compilers
+{
+    [Export(typeof(IVsTextViewCreationListener))]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [ContentType("LESS")]
+    //[ContentType("SASS")]
 
-//    class CompilationChainer : IVsTextViewCreationListener
-//    {
-//        [Import]
-//        public IVsEditorAdaptersFactoryService EditorAdaptersFactoryService { get; set; }
-//        [Import]
-//        public ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
+    class CompilationChainer : IVsTextViewCreationListener
+    {
+        [Import]
+        public IVsEditorAdaptersFactoryService EditorAdaptersFactoryService { get; set; }
+        [Import]
+        public ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
 
-//        public void VsTextViewCreated(IVsTextView textViewAdapter)
-//        {
-//            var textView = EditorAdaptersFactoryService.GetWpfTextView(textViewAdapter);
-//            var contentType = textView.TextBuffer.ContentType;
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            var textView = EditorAdaptersFactoryService.GetWpfTextView(textViewAdapter);
+            var contentType = textView.TextBuffer.ContentType;
 
-//            ITextDocument document;
-//            if (!TextDocumentFactoryService.TryGetTextDocument(textView.TextDataModel.DocumentBuffer, out document))
-//                return;
+            ITextDocument document;
+            if (!TextDocumentFactoryService.TryGetTextDocument(textView.TextDataModel.DocumentBuffer, out document))
+                return;
 
-//            var settings = WESettings.Instance.ForContentType<IChainableCompilerSettings>(contentType);
+            var settings = WESettings.Instance.ForContentType<IChainableCompilerSettings>(contentType);
 
-//            var notifierProvider = Mef.GetImport<ICompilationNotifierProvider>(contentType);
-//            var notifier = notifierProvider.GetCompilationNotifier(document);
+            var notifierProvider = Mef.GetImport<ICompilationNotifierProvider>(contentType);
+            var notifier = notifierProvider.GetCompilationNotifier(document);
 
-//            var compilerProvider = Mef.GetImport<ICompilerRunnerProvider>(contentType);
-//            var compilerRunner = compilerProvider.GetCompiler(contentType);
+            var compilerProvider = Mef.GetImport<ICompilerRunnerProvider>(contentType);
+            var compilerRunner = compilerProvider.GetCompiler(contentType);
 
-//            var graph = GetGraph(contentType);
-//            notifier.CompilationReady += async (s, e) =>
-//            {
-//                if (!settings.CompileOnSave || !settings.EnableChainCompilation)
-//                    return;
+            var graph = GetGraph(contentType);
+            notifier.CompilationReady += async (s, e) =>
+            {
+                if (!settings.CompileOnSave || !settings.EnableChainCompilation)
+                    return;
 
-//                var count = 0;
-//                foreach (var file in await graph.GetRecursiveDependentsAsync(e.CompilerResult.SourceFileName))
-//                {
-//                    if (File.Exists(compilerRunner.GetTargetPath(file)))
-//                    {
-//                        compilerRunner.CompileToDefaultOutputAsync(file).DontWait("compiling " + file);
-//                        count++;
-//                    }
-//                }
-//                EditorExtensionsPackage.DTE.StatusBar.Text = "Compiling " + count + " dependent file" + (count == 1 ? "s" : "")
-//                                                           + " for " + Path.GetFileName(e.CompilerResult.SourceFileName);
-//            };
-//        }
+                var count = 0;
+                foreach (var file in await graph.GetRecursiveDependentsAsync(e.CompilerResult.SourceFileName))
+                {
+                    if (File.Exists(compilerRunner.GetTargetPath(file)))
+                    {
+                        compilerRunner.CompileToDefaultOutputAsync(file).DontWait("compiling " + file);
+                        count++;
+                    }
+                }
+                EditorExtensionsPackage.DTE.StatusBar.Text = "Compiling " + count + " dependent file" + (count == 1 ? "s" : "")
+                                                           + " for " + Path.GetFileName(e.CompilerResult.SourceFileName);
+            };
+        }
 
-//        readonly HashSet<IContentType> registeredContentTypes = new HashSet<IContentType>();
-//        private DependencyGraph GetGraph(IContentType contentType)
-//        {
-//            var graph = (VsDependencyGraph)Mef.GetImport<DependencyGraph>(contentType);
-//            if (!registeredContentTypes.Add(contentType))
-//                return graph;
+        readonly HashSet<IContentType> registeredContentTypes = new HashSet<IContentType>();
+        private DependencyGraph GetGraph(IContentType contentType)
+        {
+            var graph = (VsDependencyGraph)Mef.GetImport<DependencyGraph>(contentType);
+            if (!registeredContentTypes.Add(contentType))
+                return graph;
 
-//            // Add this event handler only once per ContentType
-//            var settings = WESettings.Instance.ForContentType<IChainableCompilerSettings>(contentType);
-//            graph.IsEnabled = settings.EnableChainCompilation;
-//            settings.EnableChainCompilationChanged += delegate { graph.IsEnabled = settings.EnableChainCompilation; };
+            // Add this event handler only once per ContentType
+            var settings = WESettings.Instance.ForContentType<IChainableCompilerSettings>(contentType);
+            graph.IsEnabled = settings.EnableChainCompilation;
+            settings.EnableChainCompilationChanged += delegate { graph.IsEnabled = settings.EnableChainCompilation; };
 
-//            return graph;
-//        }
-//    }
-//}
+            return graph;
+        }
+    }
+}
