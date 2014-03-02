e351da8769516b37b7f8f275b3addf5d6c0dc482
Mads Kristensen <post@madskristensen.net>
2014-03-01 23:44:36 -0800

    CSS: Added color swatches to completion
    This adds a color swatch in the completion list for known color names

---
+++ EditorExtensions/CSS/Completion/Filter/ColorSwatchCompletionListFilter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.CSS.Editor.Intellisense;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor.Intellisense;
+
+namespace MadsKristensen.EditorExtensions.Css
+{
+    [Export(typeof(ICssCompletionListFilter))]
+    [Name("Color Swatch Filter")]
+    internal class ColorSwatchCompletionListFilter : ICssCompletionListFilter
+    {
+        public void FilterCompletionList(IList<CssCompletionEntry> completions, CssCompletionContext context)
+        {
+            if (context.ContextType != CssCompletionContextType.PropertyValue)
+                return;
+
+            foreach (CssCompletionEntry entry in completions)
+            {
+                var color = System.Drawing.Color.FromName(entry.DisplayText);
+
+                if (color.IsKnownColor)
+                {
+                    if (color.IsSystemColor)
+                        entry.FilterType = CompletionEntryFilterTypes.NeverVisible;
+                    else
+                        entry.IconSource = GetColorSwatch(entry.DisplayText);
+                }
+            }
+        }
+
+        private ImageSource GetColorSwatch(string value)
+        {
+            Color color = (Color)ColorConverter.ConvertFromString(value);
+
+            GeometryGroup group = new GeometryGroup();
+            group.Children.Add(new RectangleGeometry(new Rect(new Size(50, 50))));
+
+            GeometryDrawing drawing = new GeometryDrawing()
+            {
+                Geometry = group,
+                Brush = new SolidColorBrush(color),
+                Pen = new Pen(Brushes.Transparent, 10),
+            };
+
+            DrawingImage image = new DrawingImage(drawing);
+            image.Freeze();
+
+            return image;
+        }
+    }
+}
