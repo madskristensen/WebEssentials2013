be8c8c46adab4486a4178bf3462a98567225f10b
Mads Kristensen <post@madskristensen.net>
2014-03-29 11:53:38 -0700

    CSS: Filter for uncommon property names

---
+++ EditorExtensions/CSS/Completion/Filter/HideUncommonCompletionListFilter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.Composition;
+using Microsoft.CSS.Editor.Intellisense;
+using Microsoft.CSS.Editor.Schemas;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor.Intellisense;
+
+namespace MadsKristensen.EditorExtensions.Css
+{
+    [Export(typeof(ICssCompletionListFilter))]
+    [Name("HideUncommonCompletionListFilter")]
+    internal class HideUncommonCompletionListFilter : ICssCompletionListFilter
+    {
+        private static readonly StringCollection _cache = new StringCollection() 
+        { 
+            "widows", // Rarely used and get's in the way of "width"
+        };
+
+        public void FilterCompletionList(IList<CssCompletionEntry> completions, CssCompletionContext context)
+        {
+            if (context.ContextType != CssCompletionContextType.PropertyName)
+                return;
+
+            foreach (CssCompletionEntry entry in completions)
+            {
+                if (_cache.Contains(entry.DisplayText))
+                {
+                    entry.FilterType = CompletionEntryFilterTypes.NeverVisible;
+                }
+            }
+        }
+    }
+}
