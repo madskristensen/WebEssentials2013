d19638e615090ec29ef5fb1812b1bdb8b4ba1346
Mads Kristensen <post@madskristensen.net>
2014-04-12 20:13:33 -0700

    Adding JSON schema references

--- EditorExtensions/JSON/Schemas/SchemaExporter.cs
+++ EditorExtensions/JSON/Schemas/SchemaExporter.cs
@@ -11,6 +11,12 @@ namespace MadsKristensen.EditorExtensions.JSON
         private static Dictionary<string, string> _schemas = new Dictionary<string, string> 
         { 
             {".jshintrc", "http://vswebessentials.com/schemas/v0.1/jshintrc.json"},
+            {".bowerrc", "http://vswebessentials.com/schemas/v0.1/bowerrc.json"},
+            {"bower.json", "http://vswebessentials.com/schemas/v0.1/bower.json"},
+            {"package.json", "http://vswebessentials.com/schemas/v0.1/package.json"},
+            {"tslint.json", "http://vswebessentials.com/schemas/v0.1/tslint.json"},
+            {"jscsrc.json", "http://vswebessentials.com/schemas/v0.1/jscsrc.json"},
+            {".jscsrc", "http://vswebessentials.com/schemas/v0.1/jscsrc.json"},
         };
 
         public IEnumerable<string> GetAvailableSchemas()
