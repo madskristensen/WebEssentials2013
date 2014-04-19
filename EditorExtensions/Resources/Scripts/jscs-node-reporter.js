e8e7d3ca9861f9e1731ee1f27c1f2bed3e6a3faa
Adeel <adeelbm@outlook.com>
2014-04-19 17:56:36 +0300

    Code: JsHint fixes in resource files. Minor improvements.

--- EditorExtensions/Resources/Scripts/jscs-node-reporter.js
+++ EditorExtensions/Resources/Scripts/jscs-node-reporter.js
@@ -2,19 +2,23 @@
 "use strict";
 
 module.exports = function (errorCollection) {
-    var errorItems = errorCollection.map(function (errors) {
-        if (!errors.isEmpty()) {
-            errors.getErrorList().forEach(function (error) {
-                var fileName = error.getFilename();
+    var errorItems = [];
 
+    if (errorCollection[0]) {
+        var errors = errorCollection[0];
+        var fileName = errors.getFilename();
+
+        if (!errors.isEmpty()) {
+            errorItems = errors.getErrorList().map(function (error) {
                 return {
-                    Line: error.line
-                  , Column: error.column + 1
-                  , Message: "JSCS: " + error.message
-                  , FileName: fileName
+                    Line: error.line,
+                    Column: error.column + 1,
+                    Message: "JSCS: " + error.message,
+                    FileName: fileName
                 };
             });
         }
-    });
+    }
+
     process.stdout.write(JSON.stringify(errorItems));
 };
