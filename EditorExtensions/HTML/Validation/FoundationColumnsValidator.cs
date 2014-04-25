10d9fc99075141834be2142c76a7799c232b7750
gproulx <gproulx@ghislainproulx.net>
2014-04-24 21:48:06 -0400

    Add column validator for row

---
+++ EditorExtensions/HTML/Validation/FoundationColumnsValidator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Globalization;
+using System.Linq;
+using MadsKristensen.EditorExtensions.Settings;
+using Microsoft.Html.Core;
+using Microsoft.Html.Editor.Validation.Validators;
+using Microsoft.Html.Validation;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor;
+
+namespace MadsKristensen.EditorExtensions.Html
+{
+    [Export(typeof(IHtmlElementValidatorProvider))]
+    [ContentType(HtmlContentTypeDefinition.HtmlContentType)]
+    public class FoundationColumnsValidatorProvider : BaseHtmlElementValidatorProvider<FoundationColumnsValidator>
+    { }
+
+    public class FoundationColumnsValidator : BaseValidator
+    {
+        private static string _errorRowMissing = "Foundation: When using \"{0}\", you must also specify the class \"row\" on the parent element.";
+
+        public override IList<IHtmlValidationError> ValidateElement(ElementNode element)
+        {
+            var results = new ValidationErrorCollection();
+
+            if (!WESettings.Instance.Html.EnableFoundationValidation)
+                return results;
+
+            var elementClasses = element.GetAttribute("class");
+            bool useNameColumn;
+            bool useNameColumns;
+
+            // 'column' and 'columns' are allowed... unfortunately for this validator: lot of duplicate checks
+            if (elementClasses == null)
+            {
+                return results;
+            }
+            else
+            {
+                useNameColumn = elementClasses.Value.Split(' ').Any(x => x.Contains("column"));
+                useNameColumns = elementClasses.Value.Split(' ').Any(x => x.Contains("columns"));
+
+                if (!useNameColumn && !useNameColumns)
+                    return results;
+            }
+
+            var columnNameUsed = useNameColumn ? "column" : "columns";
+
+            // Foundation grid system require a direct parent <div class='row ... to work
+            if (IsParentDivElementMissingRowClass(element))
+            {
+                int index = element.Attributes.IndexOf(elementClasses);
+                string error = string.Format(CultureInfo.CurrentCulture, _errorRowMissing, columnNameUsed, "");
+
+                results.AddAttributeError(element, error, HtmlValidationErrorLocation.AttributeValue, index);
+            }
+
+            return results;
+        }
+
+        private static bool IsParentDivElementMissingRowClass(ElementNode element)
+        {
+            bool isRowClassPresent = false;
+
+            if (element.Parent == null || string.IsNullOrEmpty(element.Parent.Name))
+                return false; // Don't want false alert so better to suppose that it's a partial view of the 'row' class on the parent view.
+
+            var classNames = element.Parent.GetAttribute("class");
+            if (classNames != null && classNames.Value.Split(' ').Any(x => x.Equals("row")))
+                isRowClassPresent = true;
+
+            if (isRowClassPresent)
+            {
+                return false;
+            }
+            else
+            {
+                if (element.Parent.Name == "html")
+                {
+                    // Now at the top and no row class on this element. Confirm, it's missing. 
+                    return true;
+                }
+
+                // For the grid system of Foundation, the row class must be on the direct parent element.
+                return true;
+            }
+        }
+    }
+}
---
+++ WebEssentialsTests/Tests/Foundation/FoundationColumnsValidatorTests.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections.Generic;
+using MadsKristensen.EditorExtensions.Html;
+using Microsoft.Html.Core;
+using Microsoft.Html.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Web.Core;
+
+namespace WebEssentialsTests
+{
+    [TestClass]
+    public class FoundationColumnsValidatorTests
+    {
+        [TestMethod]
+        public void NoColumnsNoWarnings()
+        {
+            FoundationColumnsValidator validator = new FoundationColumnsValidator();
+
+            var source = @"<div class='someClass'>
+                            <i class='someOtherClass'></i>
+                        </div>";
+
+            var tree = new HtmlTree(new TextStream(source));
+
+            tree.Build();
+
+            IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0]);
+
+            int expected = 0;
+
+            Assert.AreEqual(expected, compiled.Count);
+        }
+
+        [TestMethod]
+        public void CorrectColumnsUsage()
+        {
+            FoundationColumnsValidator validator = new FoundationColumnsValidator();
+
+            var source = @"<div class='row'>
+                              <div class='medium-2 columns'>2 columns</div>
+                              <div class='medium-10 columns'>10 columns</div>
+                           </div>";
+
+            var tree = new HtmlTree(new TextStream(source));
+
+            tree.Build();
+
+            IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0].Children[0]);
+
+            int expected = 0;
+
+            Assert.AreEqual(expected, compiled.Count);
+        }
+
+        [TestMethod]
+        public void RowClassMustBeOnDirectParentElement()
+        {
+            FoundationColumnsValidator validator = new FoundationColumnsValidator();
+
+            var source = @"<div class='row'>
+                              <div>
+                                 <div class='medium-2 columns'>2 columns</div>
+                                 <div class='medium-10 columns'>10 columns</div>
+                              </div>
+                           </div>";
+
+            var tree = new HtmlTree(new TextStream(source));
+
+            tree.Build();
+
+            IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0].Children[0].Children[0]);
+
+            int expected = 1;
+
+            Assert.AreEqual(expected, compiled.Count);
+        }
+
+        [TestMethod]
+        public void NoWarningIfNoParentForColumns()
+        {
+            FoundationColumnsValidator validator = new FoundationColumnsValidator();
+
+            var source = @"<div class='medium-2 columns'>2 columns</div>
+                           <div class='medium-10 columns'>10 columns</div>";
+
+            var tree = new HtmlTree(new TextStream(source));
+
+            tree.Build();
+
+            IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0]);
+
+            int expected = 0;
+
+            Assert.AreEqual(expected, compiled.Count);
+        }
+
+
+        //[TestMethod]
+        //public void ComplexCorrectColumnsUsage()
+        //{
+        //    FoundationColumnsValidator validator = new FoundationColumnsValidator();
+
+        //    var source = @"<div class='row'>
+        //                     <div class='col-md-3 col-sm-6 col-xs-11'>               
+        //                        <b>Title 1</b>
+        //                    </div>
+        //                     <div class='col-md-8 col-md-offset-1 col-sm-6 col-xs-1'>               
+        //                        <b>Title 2</b>
+        //                    </div>
+        //                </div>";
+
+        //    var tree = new HtmlTree(new TextStream(source));
+
+        //    tree.Build();
+
+        //    IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0].Children[0]);
+
+        //    int expected = 0;
+
+        //    Assert.AreEqual(expected, compiled.Count);
+        //}
+
+        //[TestMethod]
+        //public void MustWorkForNonDivElementsToo()
+        //{
+        //    FoundationColumnsValidator validator = new FoundationColumnsValidator();
+
+        //    var source = @"<div class='form-group row'>
+        //                      <label for='commentcontent' class='control-label col-sm-2'>Comment (no HTML allowed)</label>
+        //                      <div class='col-sm-10'>
+        //                         <textarea id='commentcontent' class='form-control' rows='4' placeholder='Comment' required></textarea>
+        //                      </div>
+        //                   </div>";
+
+        //    var tree = new HtmlTree(new TextStream(source));
+
+        //    tree.Build();
+
+        //    IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0].Children[0]);
+
+        //    int expected = 0;
+
+        //    Assert.AreEqual(expected, compiled.Count);
+        //}
+
+        //[TestMethod]
+        //public void WarnIfParentsElementIsMissingRowClass()
+        //{
+        //    FoundationColumnsValidator validator = new FoundationColumnsValidator();
+
+        //    var source = @"<html>
+        //                    <body>
+        //                    <div class='someClass'>
+        //                     <div class='col-md-8'>               
+        //                        <b>Title 1</b>
+        //                    </div>
+        //                     <div class='col-md-4'>               
+        //                        <b>Title 2</b>
+        //                    </div>
+        //                </div>
+        //                </body>
+        //                </html>";
+
+        //    var tree = new HtmlTree(new TextStream(source));
+
+        //    tree.Build();
+
+        //    IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0].Children[0].Children[0].Children[0]);
+
+        //    int expected = 1;
+
+        //    Assert.AreEqual(expected, compiled.Count);
+        //    Assert.IsTrue(compiled[0].Message.Contains("col-md-8"));
+        //}
+
+        //[TestMethod]
+        //public void ErrorMsgIfMoreThanTwelveColumns()
+        //{
+        //    FoundationColumnsValidator validator = new FoundationColumnsValidator();
+
+        //    var source = @"<div class='row'>
+        //                     <div class='col-md-10'>               
+        //                        <b>Title 1</b>
+        //                    </div>
+        //                     <div class='col-md-3'>               
+        //                        <b>Title 2</b>
+        //                    </div>
+        //                </div>";
+
+        //    var tree = new HtmlTree(new TextStream(source));
+
+        //    tree.Build();
+
+        //    IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0].Children[0]);
+
+        //    int expected = 1;
+
+        //    Assert.AreEqual(expected, compiled.Count);
+        //    Assert.IsTrue(compiled[0].Message.Contains("must not exceed 12"));
+        //    Assert.IsTrue(compiled[0].Message.Contains("col-md-*"));
+        //}
+    }
+}
