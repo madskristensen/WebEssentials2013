bc56e67ab11dfbb7468c89d0e52301fbe92473db
gproulx <gproulx@ghislainproulx.net>
2014-04-24 22:35:35 -0400

    Add classes validator for Zurb Foundation

---
+++ EditorExtensions/HTML/Validation/FoundationClassValidator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Globalization;
+using System.Linq;
+using MadsKristensen.EditorExtensions.Settings;
+using Microsoft.Html.Core;
+using Microsoft.Html.Editor.Validation.Validators;
+using Microsoft.Html.Validation;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor;
+
+namespace MadsKristensen.EditorExtensions.Html
+{
+    [Export(typeof(IHtmlElementValidatorProvider))]
+    [ContentType(HtmlContentTypeDefinition.HtmlContentType)]
+    public class FoundationClassValidatorProvider : BaseHtmlElementValidatorProvider<FoundationClassValidator>
+    { }
+
+    public class FoundationClassValidator : BaseValidator
+    {
+        private static string _errorMissingColumns = "When using \"small-#\", \"medium-#\" or \"large-#\", you must also specify the class \"columns\".";
+        private static string _errorMissingSize = "When using \"columns\", you must also specify the class \"small-#\", \"medium-#\" or \"large-#\".";
+
+        public override IList<IHtmlValidationError> ValidateElement(ElementNode element)
+        {
+            var results = new ValidationErrorCollection();
+
+            if (!WESettings.Instance.Html.EnableFoundationValidation)
+                return results;
+
+            var classNames = element.GetAttribute("class");
+
+            if (classNames == null)
+                return results;
+
+            if (!ColumnPairElementsOk(classNames.Value))
+            {
+                int index = element.Attributes.IndexOf(classNames);
+                var specificErrorMessage = classNames.Value.Contains("column") ? _errorMissingSize : _errorMissingColumns;
+
+                results.AddAttributeError(element, specificErrorMessage, HtmlValidationErrorLocation.AttributeValue, index);
+            }
+
+            return results;
+        }
+
+        private bool ColumnPairElementsOk(string input)
+        {
+            if (input.Contains("columns") || input.Contains("column")) {
+
+                if (input.Contains("small-") || input.Contains("medium-") || input.Contains("large-"))
+                {
+                    // Both elements are there
+                    return true;
+                }
+
+                // Size is missing
+                return false;
+            }
+            else
+            {
+                if (input.Contains("small-") || input.Contains("medium-") || input.Contains("large-"))
+                {
+                    // Size w/o columns. 
+                    return false;
+                }
+            }
+            
+            // No columns elements. OK
+            return true;
+        }
+    }
+}
---
+++ WebEssentialsTests/Tests/Foundation/FoundationValidationTests.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using MadsKristensen.EditorExtensions.Html;
+using Microsoft.Html.Core;
+using Microsoft.Html.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Web.Core;
+
+
+namespace WebEssentialsTests
+{
+    [TestClass]
+    public class FoundationValidationTests
+    {
+        [TestMethod]
+        public void ValidColumnDeclaration()
+        {
+            FoundationClassValidator validator = new FoundationClassValidator();
+
+            var source = @"<div class='small-2 columns'>2 columns</div>";
+
+            var tree = new HtmlTree(new TextStream(source));
+
+            tree.Build();
+
+            IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0]);
+
+            int expected = 0;
+
+            Assert.AreEqual(expected, compiled.Count);
+        }
+
+        [TestMethod]
+        public void MissingColumnClass()
+        {
+            FoundationClassValidator validator = new FoundationClassValidator();
+
+            var source = @"<div class='small-2'>2 columns</div>";
+
+            var tree = new HtmlTree(new TextStream(source));
+
+            tree.Build();
+
+            IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0]);
+
+            int expected = 1;
+            string expectedMessagePart = "When using \"small-#\"";
+
+            Assert.AreEqual(expected, compiled.Count);
+            Assert.IsTrue(compiled[0].Message.Contains(expectedMessagePart));
+            System.Console.WriteLine(compiled[0].Message);
+        }
+
+        [TestMethod]
+        public void MissingSizeClass()
+        {
+            FoundationClassValidator validator = new FoundationClassValidator();
+
+            var source = @"<div class='columns'>2 columns</div>";
+
+            var tree = new HtmlTree(new TextStream(source));
+
+            tree.Build();
+
+            IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0]);
+
+            int expected = 1;
+            string expectedMessagePart = "When using \"columns\"";
+
+            Assert.AreEqual(expected, compiled.Count);
+            Assert.IsTrue(compiled[0].Message.Contains(expectedMessagePart));
+            System.Console.WriteLine(compiled[0].Message);
+        }
+
+        [TestMethod]
+        public void FoundationClassValidatorDoNothingIfClassAttributIsMissing()
+        {
+            FoundationClassValidator validator = new FoundationClassValidator();
+
+            var source = @"<div>2 columns</div>";
+
+            var tree = new HtmlTree(new TextStream(source));
+
+            tree.Build();
+
+            IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0]);
+
+            int expected = 0;
+
+            Assert.AreEqual(expected, compiled.Count);
+        }
+
+        [TestMethod]
+        public void FoundationClassValidatorDoNothingIfClassAttributContainsNothingAboutColumns()
+        {
+            FoundationClassValidator validator = new FoundationClassValidator();
+
+            var source = @"<div class='somethingElse'>2 columns</div>";
+
+            var tree = new HtmlTree(new TextStream(source));
+
+            tree.Build();
+
+            IList<IHtmlValidationError> compiled = validator.ValidateElement(tree.RootNode.Children[0]);
+
+            int expected = 0;
+
+            Assert.AreEqual(expected, compiled.Count);
+        }
+
+    }
+}
