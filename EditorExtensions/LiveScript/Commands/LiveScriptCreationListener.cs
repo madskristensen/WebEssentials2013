6ee24713fcc4db9219eb3e8eceeb39d165e451a1
Frank Dumont <fj.dumont@gmail.com>
2014-05-02 15:28:04 +0200

    Added LiveScript creation listener

---
+++ EditorExtensions/LiveScript/Commands/LiveScriptCreationListener.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.Composition;
+using MadsKristensen.EditorExtensions.Settings;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor;
+
+namespace MadsKristensen.EditorExtensions.LiveScript
+{
+    [Export(typeof(IVsTextViewCreationListener))]
+    [ContentType(LiveScriptContentTypeDefinition.LiveScriptContentType)]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    public class LiveScriptViewCreationListener : IVsTextViewCreationListener
+    {
+        [Import]
+        public IVsEditorAdaptersFactoryService EditorAdaptersFactoryService { get; set; }
+
+        [Import]
+        public ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            var textView = EditorAdaptersFactoryService.GetWpfTextView(textViewAdapter);
+
+            textView.Properties.GetOrCreateSingletonProperty(() => new EnterIndentation(textViewAdapter, textView));
+            textView.Properties.GetOrCreateSingletonProperty(() => new CommentCommandTarget(textViewAdapter, textView, "#"));
+
+            //ITextDocument document;
+            //if (TextDocumentFactoryService.TryGetTextDocument(textView.TextDataModel.DocumentBuffer, out document))
+            //{
+            //    var lintInvoker = new LintFileInvoker(
+            //        f => new LintReporter(new CoffeeLintCompiler(), WESettings.Instance.CoffeeScript, f),
+            //        document
+            //    );
+            //    textView.Closed += (s, e) => lintInvoker.Dispose();
+
+            //    textView.TextBuffer.Properties.GetOrCreateSingletonProperty(() => lintInvoker);
+            //}
+        }
+    }
+
+}
