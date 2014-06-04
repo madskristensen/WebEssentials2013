c255d720dc09494d0c880fa774bf5c5d612cdf76
Mads Kristensen <post@madskristensen.net>
2014-06-03 17:19:45 -0700

    JSON: Intellisense for "required" values

---
+++ EditorExtensions/JSON/Completion/RequireCompletionProvider.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.JSON.Core.Parser;
+using Microsoft.JSON.Editor.Completion;
+using Microsoft.JSON.Editor.Completion.Def;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor;
+
+namespace MadsKristensen.EditorExtensions.JSON
+{
+    [Export(typeof(IJSONCompletionListProvider))]
+    [Name("RequireCompletionProvider")]
+    internal class RequireCompletionProvider : IJSONCompletionListProvider
+    {
+        public JSONCompletionContextType ContextType
+        {
+            get { return JSONCompletionContextType.ArrayElement; }
+        }
+
+        public IEnumerable<JSONCompletionEntry> GetListEntries(JSONCompletionContext context)
+        {
+            JSONMember req = context.ContextItem.FindType<JSONMember>();
+
+            if (req == null || req.Name == null || req.Name.Text != "\"required\"")
+                yield break;
+                        
+            var propVisitor = new JSONItemCollector<JSONMember>();
+            req.Parent.Accept(propVisitor);
+
+            var props = propVisitor.Items.FirstOrDefault(i => i.Name.Text == "\"properties\"");
+
+            if (props == null)
+                yield break;
+
+            var child = props.Children[2] as JSONBlockItem;
+
+            if (child == null)
+                yield break;
+
+            var visitor = new JSONItemCollector<JSONMember>();
+            child.Accept(visitor);
+
+            foreach (var item in visitor.Items)
+            {                
+                yield return new JSONCompletionEntry(item.Name.Text.Trim('"'), item.Name.Text, null,
+                GlyphService.GetGlyph(StandardGlyphGroup.GlyphGroupVariable, StandardGlyphItem.GlyphItemPublic),
+                "iconAutomationText", true, context.Session as ICompletionSession);
+            }            
+        }
+    }
+}
