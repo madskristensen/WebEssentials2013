d07d0a24aad7798d48a02183c9764515c3a59007
Mads Kristensen <post@madskristensen.net>
2014-08-28 08:02:02 -0700

    HTML: Fixed type ambiguity issue in CI builds

--- EditorExtensions/HTML/Completion/SimpleHtmlCompletion.cs
+++ EditorExtensions/HTML/Completion/SimpleHtmlCompletion.cs
@@ -1,19 +1,19 @@
 ﻿using System.Windows.Media;
 using Microsoft.Html.Editor.Intellisense;
-using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.Web.Editor;
+using vs = Microsoft.VisualStudio.Language.Intellisense;
 
 namespace MadsKristensen.EditorExtensions.Html
 {
     public class SimpleHtmlCompletion : HtmlCompletion
     {
-        private static ImageSource _glyph = GlyphService.GetGlyph(StandardGlyphGroup.GlyphGroupVariable, StandardGlyphItem.GlyphItemPublic);
+        private static ImageSource _glyph = GlyphService.GetGlyph(vs.StandardGlyphGroup.GlyphGroupVariable, vs.StandardGlyphItem.GlyphItemPublic);
 
-        public SimpleHtmlCompletion(string displayText, ICompletionSession session)
+        public SimpleHtmlCompletion(string displayText, vs.ICompletionSession session)
             : base(displayText, displayText, null, _glyph, HtmlIconAutomationText.AttributeIconText, session)
         { }
 
-        public SimpleHtmlCompletion(string displayText, string description, ICompletionSession session)
+        public SimpleHtmlCompletion(string displayText, string description, vs.ICompletionSession session)
             : base(displayText, displayText, description, _glyph, HtmlIconAutomationText.AttributeIconText, session)
         { }
     }
