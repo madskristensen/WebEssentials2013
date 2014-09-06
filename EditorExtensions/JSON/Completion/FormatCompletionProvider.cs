c7dc1f56b5d1d20d97dce5e7924a360389dc2d91
Mads Kristensen <post@madskristensen.net>
2014-09-06 10:42:17 -0700

    JSON: Added completion for "format" property values

---
+++ EditorExtensions/JSON/Completion/FormatCompletionProvider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Microsoft.JSON.Core.Parser;
+using Microsoft.JSON.Editor.Completion;
+using Microsoft.JSON.Editor.Completion.Def;
+using Microsoft.VisualStudio.Utilities;
+
+namespace MadsKristensen.EditorExtensions.JSON
+{
+    [Export(typeof(IJSONCompletionListProvider))]
+    [Name("FormatCompletionProvider")]
+    internal class FormatCompletionProvider : IJSONCompletionListProvider
+    {
+        private static List<string> _props = new List<string>
+        {
+            "date-time",
+            "email",
+            "hostname",
+            "ipv4",
+            "ipv6",
+            "regex",
+            "uri",
+         };
+
+        public JSONCompletionContextType ContextType
+        {
+            get { return JSONCompletionContextType.PropertyValue; }
+        }
+
+        public IEnumerable<JSONCompletionEntry> GetListEntries(JSONCompletionContext context)
+        {
+            JSONMember member = context.ContextItem as JSONMember;
+
+            if (member == null || member.Name == null || member.UnquotedNameText != "format")
+                yield break;
+
+            foreach (string prop in _props)
+            {
+                yield return new SimpleCompletionEntry(prop, "\"" + prop + "\"", context.Session);
+            }
+        }
+    }
+}
