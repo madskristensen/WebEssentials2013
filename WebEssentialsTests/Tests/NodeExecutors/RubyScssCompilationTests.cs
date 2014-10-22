cad9e32d2105768f0174889cea3c7d80cb1ac4f5
David <david@teasdale.me>
2014-10-22 16:49:07 +0100

    Add ruby scss tests

--- WebEssentialsTests/Tests/NodeExecutors/RubyScssCompilationTests.cs
+++ WebEssentialsTests/Tests/NodeExecutors/RubyScssCompilationTests.cs
@@ -1,15 +1,21 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using FluentAssertions;
 using MadsKristensen.EditorExtensions.Scss;
 using MadsKristensen.EditorExtensions.Settings;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace WebEssentialsTests
 {
     [TestClass]
     public class RubyScssCompilationTests
     {
+        private static readonly string BaseDirectory = Path.GetDirectoryName(typeof(ScssCompilationTests).Assembly.Location);
+        private static readonly Regex BlockComments = new Regex(@"/\*(.*?)\*/", RegexOptions.IgnoreCase);
+
         [ClassInitialize]
         public static void Initialize(TestContext c)
         {
@@ -23,20 +29,26 @@ namespace WebEssentialsTests
             });
         }
 
-        private static readonly string BaseDirectory = Path.GetDirectoryName(typeof(ScssCompilationTests).Assembly.Location);
-
         [TestMethod]
         public async Task RubyScssBasicCompilationTest()
         {
-            foreach (var sourceFile in Directory.EnumerateFiles(Path.Combine(BaseDirectory, "fixtures", "ruby-scss"), "*.scss", SearchOption.AllDirectories))
+            foreach (var sourceFile in Directory.EnumerateFiles(Path.Combine(BaseDirectory, "fixtures", "ruby-scss", "source"), "*.scss"))
             {
-                var compiledFile = Path.ChangeExtension(sourceFile, ".css");
+                var compiledFile = Path.ChangeExtension(sourceFile, ".css").Replace("source\\", "compiled\\");
 
-                var expected = File.ReadAllText(compiledFile).Replace("\r", "").Replace("\n", "").Replace(" ", "");
+                var expected = CleanCss(File.ReadAllText(compiledFile));
                 var compiled = await new ScssCompiler().CompileToStringAsync(sourceFile);
 
-                compiled.Replace("\r", "").Replace("\n", "").Replace(" ", "").Trim().Should().Be(expected.Trim());
+                CleanCss(compiled).Should().Be(expected);
             }
+        }
+
+        private static string CleanCss(string css)
+        {
+            css = css.Replace("\n", "").Replace("\r", "").Replace(" ", "");
+            css = BlockComments.Replace(css, "");
+
+            return css.Trim();
         }
     }
 }
