743df646966001eeb84752a79b7590d08e6ac9d3
Adeel <adeelbm@outlook.com>
2015-07-18 08:02:09 +0300

    Package: Updates version to 2.6.13.

--- EditorExtensions/WebEssentialsPackage.cs
+++ EditorExtensions/WebEssentialsPackage.cs
@@ -48,7 +48,7 @@ namespace MadsKristensen.EditorExtensions
     [SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling"), PackageRegistration(UseManagedResourcesOnly = true)]
     public sealed class WebEssentialsPackage : Package
     {
-        public const string Version = "2.6.0.0";
+        public const string Version = "2.6.13.0";
 
         private static DTE2 _dte;
         private static IVsRegisterPriorityCommandTarget _pct;
